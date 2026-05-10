8f076c200da156db38035d1f347da4d7ddbfa32d
Pikcube <michaelds13@gmail.com>
2026-05-10 19:40:54 -0400

    Added Strikes and Defends

---
+++ TheInventor/TheInventorCode/Cards/DefendInventor.cs
@@ -0,0 +1,27 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheInventor.TheInventorCode.Keywords;
+
+namespace TheInventor.TheInventorCode.Cards;
+
+public class DefendInventor() : TheInventorCard(1, CardType.Skill, CardRarity.Basic, TargetType.Self)
+{
+    public override bool GainsBlock => true;
+
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Defend];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(5M, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+
+    protected override void OnUpgrade()
+    {
+        AddKeyword(ScrapKeyword.Scrap);
+    }
+}
---
+++ TheInventor/TheInventorCode/Cards/StrikeInventor.cs
@@ -0,0 +1,30 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheInventor.TheInventorCode.Keywords;
+
+namespace TheInventor.TheInventorCode.Cards;
+
+
+public class StrikeInventor() : TheInventorCard(1, CardType.Attack, CardRarity.Basic, TargetType.AnyEnemy)
+{
+    protected override HashSet<CardTag> CanonicalTags => [CardTag.Strike];
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6M, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target, "cardPlay.Target");
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target).WithHitFx("vfx/vfx_attack_slash").Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        AddKeyword(ScrapKeyword.Scrap);
+    }
+}
