a955ded759612af40122275faa34302cfa26a4d7
Pikcube <michaelds13@gmail.com>
2026-05-19 10:18:57 -0400

    Started work on Snowflake

---
+++ TheInventor/TheInventorCode/Cards/Common/Snowflake.cs
@@ -0,0 +1,32 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheInventor.TheInventorCode.Gadgets;
+using TheInventor.TheInventorCode.Powers;
+
+namespace TheInventor.TheInventorCode.Cards.Common;
+
+public class Snowflake() : TheInventorCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+{
+    public override string OnScrap()
+    {
+        return nameof(DefaultGadget);
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(10, BlockProps.card), new PowerVar<DexterityPower>(-1)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+
+        await PowerCmd.Apply<FreezePower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(3);
+    }
+}
---
+++ TheInventor/TheInventorCode/Powers/FreezePower.cs
@@ -0,0 +1,29 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using Pikcube.Common.Extensions;
+using TheInventor.TheInventorCode.Cards.Common;
+
+namespace TheInventor.TheInventorCode.Powers;
+
+
+public class FreezePower : TheInventorPower
+{
+    public override PowerType Type => PowerType.Debuff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task BeforeApplied(Creature target, decimal amount, Creature? applier, CardModel? cardSource)
+    {
+        await PowerCmd.Apply<DexterityPower>(new BlockingPlayerChoiceContext(), target, -amount, applier, cardSource);
+    }
+
+    public override async Task AfterTurnEnd(PlayerChoiceContext choiceContext, CombatSide side)
+    {
+        await PowerCmd.Apply<DexterityPower>(new BlockingPlayerChoiceContext(), Owner, Amount, Applier, null);
+        await PowerCmd.Remove(this);
+    }
+}
