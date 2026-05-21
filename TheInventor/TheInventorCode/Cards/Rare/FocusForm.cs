4259f1e278c89f7d4f045ce9b48f529ac522cd84
Pikcube <michaelds13@gmail.com>
2026-05-21 17:16:16 -0400

    Added Focus Form stub

---
+++ TheInventor/TheInventorCode/Cards/Rare/FocusForm.cs
@@ -0,0 +1,10 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using TheInventor.TheInventorCode.Gadgets;
+
+namespace TheInventor.TheInventorCode.Cards.Rare;
+
+  
+public class FocusForm() : TheInventorCard(3, CardType.Power, CardRarity.Rare, TargetType.Self)
+{
+    public override string GetScrapId => nameof(BattleWrench);
+}
