07a15d6bff98cb2588052628cfe642baddf70eda
Pikcube <michaelds13@gmail.com>
2026-05-22 16:47:47 -0400

    Cleanup

--- TheInventor/TheInventorCode/Cards/Uncommon/ElasticHeart.cs
+++ TheInventor/TheInventorCode/Cards/Uncommon/ElasticHeart.cs
@@ -1,5 +1,4 @@
-﻿using BaseLib.Utils;
-using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
