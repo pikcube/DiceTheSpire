66b86487caa16522f2aa6a24bcb1382ef43b67ed
Cory Zink <cz109918@ohio.edu>
2026-05-25 23:55:25 -0400

    added Splinter, Blender, Detonator, Peashooter, Poison Needle

---
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Cards/Splinter.cs
@@ -0,0 +1,15 @@
+﻿using BaseLib.Utils;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Singletons;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models.CardPools;
+
+
+namespace DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+
+[Pool(typeof(StatusCardPool))]
+public class Splinter() : DiceTheSpireCoreCard(1, CardType.Status, CardRarity.Status, TargetType.None)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain, CardKeyword.Exhaust, CardKeyword.Sly];
+}
---
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Interfaces/ICountdown.cs
@@ -0,0 +1,9 @@
+﻿namespace DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+
+public abstract class ICountdown
+{
+    public int CurrentCountdown;
+    public int MaxCountdown;
+
+
+}
