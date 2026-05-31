f61aea00e3847fcbcc1e304998bf986304b32743
Cory Zink <cz109918@ohio.edu>
2026-05-31 03:16:55 -0400

    Created Dexterity Charm and Bounce. Created BouncePower

--- TheThief/TheThiefCode/Cards/Bounce.cs
+++ TheThief/TheThiefCode/Cards/Bounce.cs
@@ -1,16 +1,17 @@
-﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+﻿using BaseLib.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
 using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
+using TheThief.TheThiefCode.Powers;
 
 namespace TheThief.TheThiefCode.Cards;
 
   
-public class DexterityCharm() : TheThiefCard(-1, CardType.Power, CardRarity.Uncommon, TargetType.Self), ICountdown
+public class Bounce() : TheThiefCard(-1, CardType.Power, CardRarity.Uncommon, TargetType.Self), ICountdown
 {
     public int MaxCount
     {
@@ -21,7 +22,7 @@ public class DexterityCharm() : TheThiefCard(-1, CardType.Power, CardRarity.Unco
             field = value;
             CurrentCount += changeBy;
         }
-    } = 4;
+    } = 6;
 
     public int CurrentCount
     {
@@ -41,21 +42,21 @@ public class DexterityCharm() : TheThiefCard(-1, CardType.Power, CardRarity.Unco
         }
     }
 
-    protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new PowerVar<DexterityPower>(3M), new IntVar("CurrentCount", 4)];
 
-    protected override void OnUpgrade()
-    {
-        this.UpgradeCountdown(-1);
-    }
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new IntVar("CurrentCount", 6), new PowerVar<BouncePower>(1M)];
 
     public async Task OnCountdownZero(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<DexterityPower>(choiceContext, Owner.Creature, DynamicVars.Dexterity.BaseValue, Owner.Creature, this);
+        await PowerCmd.Apply<BouncePower>(choiceContext, Owner.Creature, DynamicVars.Power<BouncePower>().BaseValue, Owner.Creature, this);
     }
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         await Countdown.OnCountdownPlay(choiceContext, cardPlay, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.UpgradeCountdown(-2);
     }
 }
---
+++ TheThief/TheThiefCode/Powers/BouncePower.cs
@@ -0,0 +1,34 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheThief.TheThiefCode.Powers;
+
+  
+public class BouncePower : TheThiefPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override (PileType, CardPilePosition) ModifyCardPlayResultPileTypeAndPosition(CardModel card, bool isAutoPlay,
+        ResourceInfo resources, PileType pileType, CardPilePosition position)
+    {
+        if (card.Owner.Creature != this.Owner ||
+            CombatManager.Instance.History.CardPlaysStarted.Count<CardPlayStartedEntry>(
+                (Func<CardPlayStartedEntry, bool>)(e =>
+                    e.Actor == this.Owner && e.CardPlay.IsFirstInSeries && e.HappenedThisTurn(this.CombatState))) >=
+            this.Amount || pileType != PileType.Discard)
+        {
+            return (pileType, position);
+        }
+        return (PileType.Hand, CardPilePosition.Bottom);
+    }
+
+    public override Task AfterModifyingCardPlayResultPileOrPosition(CardModel card, PileType pileType, CardPilePosition position)
+    {
+        this.Flash();
+        return Task.CompletedTask;
+    }
+}
