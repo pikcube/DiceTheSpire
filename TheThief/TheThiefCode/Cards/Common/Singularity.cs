cd383adef07317798f53616c86713933f2e2f576
Cory Zink <cz109918@ohio.edu>
2026-06-02 03:19:55 -0400

    Fixed upgraded card text & hovertips for Singularity and Crowbar

--- TheThief/TheThiefCode/Cards/Common/Singularity.cs
+++ TheThief/TheThiefCode/Cards/Common/Singularity.cs
@@ -12,7 +12,7 @@ namespace TheThief.TheThiefCode.Cards.Common;
 public class Singularity() : TheThiefCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
 {
 
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [new CardHoverTip(ModelDb.Card<Collapse>())];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Collapse>(this.IsUpgraded)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
