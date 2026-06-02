1c93af8ded48dd3001e76334019bcacbfa5039e9
Cory Zink <cz109918@ohio.edu>
2026-06-02 04:13:07 -0400

    Added Blight

---
+++ TheThief/TheThiefCode/Cards/Rare/Blight.cs
@@ -0,0 +1,69 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Keywords;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheThief.TheThiefCode.Cards.Uncommon;
+
+namespace TheThief.TheThiefCode.Cards.Rare;
+
+public class Blight() : TheThiefCard(-1, CardType.Skill, CardRarity.Rare, TargetType.AnyEnemy), ICountdown
+{
+    public int MaxCount
+    {
+        get;
+        set
+        {
+            int changeBy = value - field;
+            field = value;
+            CurrentCount += changeBy;
+        }
+    } = 5;
+
+    public int CurrentCount
+    {
+        get => DynamicVars[nameof(CurrentCount)].IntValue;
+        set
+        {
+            DynamicVars[nameof(CurrentCount)].BaseValue = value;
+            if (DynamicVars[nameof(CurrentCount)].BaseValue < 0)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = 0;
+            }
+
+            if (DynamicVars[nameof(CurrentCount)].BaseValue > MaxCount)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = MaxCount;
+            }
+        }
+    }
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new IntVar(nameof(CurrentCount), 5)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CountdownModel.Countdown];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<PoisonPower>()];
+
+    public async Task OnCountdownZero(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        if (cardPlay.Target.HasPower<PoisonPower>())
+        {
+            await PowerCmd.Apply<PoisonPower>(choiceContext, cardPlay.Target, cardPlay.Target.GetPowerAmount<PoisonPower>(),
+                Owner.Creature, this);
+        }
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await Countdown.OnCountdownPlay(choiceContext, cardPlay, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.UpgradeCountdown(-2);
+    }
+}
