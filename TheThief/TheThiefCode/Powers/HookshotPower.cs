11b572e04407eef19c1c28bb06faf5d3365d163f
Cory Zink <cz109918@ohio.edu>
2026-06-02 07:16:49 -0400

    Added Hookshot

---
+++ TheThief/TheThiefCode/Powers/HookshotPower.cs
@@ -0,0 +1,41 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Combat.History.Entries;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheThief.TheThiefCode.Powers;
+
+  
+public class HookshotPower : TheThiefPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override (PileType, CardPilePosition) ModifyCardPlayResultPileTypeAndPosition(CardModel card, bool isAutoPlay,
+        ResourceInfo resources, PileType pileType, CardPilePosition position)
+    {
+        if (card.Owner.Creature != this.Owner || pileType != PileType.Discard)
+        {
+            return (pileType, position);
+        }
+        return (PileType.Hand, CardPilePosition.Bottom);
+    }
+
+    public override async Task AfterModifyingCardPlayResultPileOrPosition(CardModel card, PileType pileType, CardPilePosition position)
+    {
+        this.Flash();
+        await PowerCmd.Decrement(this);
+    }
+
+    public override async Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side == Owner.Side && Owner.HasPower<HookshotPower>())
+        {
+            await PowerCmd.Remove<HookshotPower>(Owner);
+        }
+    }
+}
