cafdbfd7fc7c30d4aa56a7bfe1f4cf3b7582decc
Cory Zink <cz109918@ohio.edu>
2026-06-02 20:07:13 -0400

    Added Hacksaw

---
+++ TheThief/TheThiefCode/Cards/Uncommon/Hacksaw.cs
@@ -0,0 +1,40 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using TheThief.TheThiefCode.Cards.Token;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class Hacksaw() : TheThiefCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        CardSelectorPrefs prefs = new(CardSelectorPrefs.TransformSelectionPrompt, 1);
+        CardModel? original = (await CardSelectCmd.FromHand(choiceContext, Owner, prefs, null, this)).FirstOrDefault();
+        if (original == null)
+        {
+            return;
+        }
+        CardModel card = CombatState.CreateCard<Pip>(Owner);
+        await CardCmd.Transform(original, card);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Cards.UpgradeValueBy(1);
+    }
+}
