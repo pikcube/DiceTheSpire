e01542745cfd5366f3c9e8bb31cce53af383a08f
Corey <coreySegiethMail@gmail.com>
2026-06-03 00:20:03 -0400

    Added Slingshot

---
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/Slingshot.cs
@@ -0,0 +1,36 @@
+﻿using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheWarrior.TheWarriorCode.Cards.Basic;
+
+public class Slingshot() : TheWarriorCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(5)];
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+
+        CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_DISCARD"), 0, this.DynamicVars.Cards.IntValue);
+        CardModel[] cards = [.. await CardSelectCmd.FromHand(choiceContext, Owner, cardSelectorPrefs, null, this)];
+        foreach (CardModel card in cards)
+        {
+            await CardCmd.Discard(choiceContext, card);
+        }
+
+        if (cards.Length == 0)
+        {
+            return;
+        }
+
+        await CardPileCmd.Draw(choiceContext, cards.Length, Owner);
+    }
+
+    protected override void OnUpgrade() => this.DynamicVars.Cards.UpgradeValueBy(3);
+
+}
