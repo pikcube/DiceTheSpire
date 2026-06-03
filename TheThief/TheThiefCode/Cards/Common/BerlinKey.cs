f394640aa83bcdfe26216d30ece21682c2f08794
Cory Zink <cz109918@ohio.edu>
2026-06-02 20:07:23 -0400

    Added Berlin Key

---
+++ TheThief/TheThiefCode/Cards/Common/BerlinKey.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.CompilerServices;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using TheThief.TheThiefCode.Cards.Token;
+
+namespace TheThief.TheThiefCode.Cards.Common;
+
+public class BerlinKey() : TheThiefCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+        CardSelectorPrefs prefs = new(CardSelectorPrefs.EnchantSelectionPrompt, 1);
+        CardModel? original = (await CardSelectCmd.FromHand(choiceContext, Owner, prefs, null, this)).FirstOrDefault();
+        if (original == null)
+        {
+            return;
+        }
+        
+        CardPileAddResult addedCard =  await CardPileCmd.AddGeneratedCardToCombat(original.CreateClone(), IsUpgraded ? PileType.Hand : PileType.Discard, Owner);
+        CardCmd.PreviewCardPileAdd(addedCard);
+    }
+}
