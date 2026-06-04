9a7eb8b4c5dafe05b27800bad0112ff05a6dd32e
Cory Zink <cz109918@ohio.edu>
2026-06-04 00:37:21 -0400

    Added Raw Ambition (Dusty Tome Power)

---
+++ TheThief/TheThiefCode/Cards/Ancient/RawAmbition.cs
@@ -0,0 +1,25 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using TheThief.TheThiefCode.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Ancient;
+
+public class RawAmbition() : TheThiefCard(2, CardType.Power, CardRarity.Ancient, TargetType.Self), ITomeCard
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1), new CardsVar(3)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [EnergyHoverTip];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<RawAmbitionPower>(choiceContext, Owner.Creature, DynamicVars.Energy.BaseValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
---
+++ TheThief/TheThiefCode/Powers/RawAmbitionPower.cs
@@ -0,0 +1,48 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class RawAmbitionPower : TheThiefPower
+{
+    private const int CardPlayCount = 3;
+    private bool _triggerOnSourcePlay = true;
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+    public override PowerInstanceType InstanceType => PowerInstanceType.Instanced;
+    protected override object InitInternalData() => new Data();
+    public override int DisplayAmount => CardPlayCount - GetInternalData<Data>().CardsPlayed % CardPlayCount;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(CardPlayCount)];
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (_triggerOnSourcePlay)
+        {
+            _triggerOnSourcePlay = false;
+            return;
+        }
+        if (cardPlay.Card.Owner.Creature != Owner || cardPlay.Resources.EnergySpent != 1)
+        {
+            return;
+        }
+
+        Data data = GetInternalData<Data>();
+        data.CardsPlayed += 1;
+        if (data.CardsPlayed >= CardPlayCount)
+        { 
+            Flash();
+            await PlayerCmd.GainEnergy(1, cardPlay.Card.Owner);
+            data.CardsPlayed -= CardPlayCount;
+        }
+        InvokeDisplayAmountChanged();
+
+    }
+
+    private class Data
+    {
+        public int CardsPlayed;
+    }
+}
