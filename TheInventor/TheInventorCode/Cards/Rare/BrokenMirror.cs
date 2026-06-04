cad87a203916997636a79868c951b7f6483e0ff1
Pikcube <michaelds13@gmail.com>
2026-06-04 00:41:13 -0400

    Added hover tip to broken mirror

--- TheInventor/TheInventorCode/Cards/Rare/BrokenMirror.cs
+++ TheInventor/TheInventorCode/Cards/Rare/BrokenMirror.cs
@@ -3,6 +3,7 @@ using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using Pikcube.Common.Commands;
 using Pikcube.Common.Extensions;
@@ -18,6 +19,8 @@ public class BrokenMirror() : TheInventorCard(1, CardType.Skill, CardRarity.Rare
     public override string GetScrapId => nameof(InfinityMirror);
 
     protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1), new PowerVar<CursedPower>(1)];
+
+    protected override IEnumerable<IHoverTip> ExtraInventorHoverTips => [HoverTipFactory.FromPower<CursedPower>()];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
