44c88ab5b2ad0a3cbdef10e43d30142e2f68d8fc
Cory Zink <cz109918@ohio.edu>
2026-06-04 01:20:35 -0400

    Added Lucky Draw

---
+++ TheThief/TheThiefCode/Powers/LuckyDrawPower.cs
@@ -0,0 +1,23 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class LuckyDrawPower : TheThiefPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        
+        if (cardPlay.Card.Owner != Owner.Player || cardPlay.Card.EnergyCost.Canonical != 1)
+        {
+            return;
+        }
+        await CardPileCmd.Draw(choiceContext, Amount, Owner.Player);
+    }
+}
