b7b339c536f3197bc71ed57460fd02c03f377ce6
Cory Zink <cz109918@ohio.edu>
2026-06-04 18:00:44 -0400

    Added Lock

---
+++ TheThief/TheThiefCode/Cards/Uncommon/Lock.cs
@@ -0,0 +1,37 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class Lock() : TheThiefCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    public override bool CanBeGeneratedInCombat => false;
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        CardModel? card = CardFactory.GetDistinctForCombat(Owner,
+            Owner.Character.CardPool.GetUnlockedCards(Owner.UnlockState, Owner.RunState.CardMultiplayerConstraint)
+                .Where(c => c.EnergyCost.Canonical == 1 && c.CanBeGeneratedInCombat), 1, Owner.RunState.Rng.CombatCardGeneration).FirstOrDefault();
+        if (card == null)
+        {
+            return;
+        }
+        await CardPileCmd.AddGeneratedCardToCombat(card, PileType.Hand, Owner);
+
+        if (IsUpgraded)
+        {
+            await PlayerCmd.GainEnergy(DynamicVars.Energy.BaseValue, Owner);
+        }
+    }
+}
