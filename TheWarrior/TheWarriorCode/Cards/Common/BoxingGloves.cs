65e49a4c14d2f21151f6623ec623674db872daca
Corey <coreySegiethMail@gmail.com>
2026-06-05 20:11:22 -0400

    Adds Boxing Gloves (Yes, I know it is using the wrong type of EnergyCost. I don't know how to fix it and I ran out of time).

---
+++ TheWarrior/TheWarriorCode/Cards/Common/BoxingGloves.cs
@@ -0,0 +1,50 @@
+﻿using JetBrains.Annotations;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheWarrior.TheWarriorCode.Cards.Basic;
+
+
+[UsedImplicitly]
+public class BoxingGloves() : TheWarriorCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars
+    {
+        get
+        {
+            return [new DamageVar(4M, DamageProps.card), new BlockVar((5), BlockProps.card)];
+        }
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx(VfxCmd.slashPath)
+            .Execute(choiceContext);
+
+        int xValue = EnergyCost.Canonical;
+
+        for (int n = 0; n < xValue; ++n)
+        {
+            await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        }
+    
+    }
+
+
+    protected override void OnUpgrade()
+    {
+        base.OnUpgrade();
+        DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars.Damage.UpgradeValueBy(1);
+    }
+
+}
