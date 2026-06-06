9654a1d65fddab8724bdadbdb2bfe97e8031bd43
Cory Zink <cz109918@ohio.edu>
2026-06-06 14:42:16 -0400

    Added SecretWeapon

---
+++ TheThief/TheThiefCode/Cards/Rare/SecretWeapon.cs
@@ -0,0 +1,58 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheThief.TheThiefCode.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Rare;
+
+public class SecretWeapon() : TheThiefCard(-1, CardType.Power, CardRarity.Rare, TargetType.Self), ICountdown
+{
+    public int MaxCount
+    {
+        get;
+        set
+        {
+            int changeBy = value - field;
+            field = value;
+            CurrentCount += changeBy;
+        }
+    } = 3;
+
+    public int CurrentCount
+    {
+        get => DynamicVars[nameof(CurrentCount)].IntValue;
+        set
+        {
+            DynamicVars[nameof(CurrentCount)].BaseValue = value;
+            if (DynamicVars[nameof(CurrentCount)].BaseValue < 0)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = 0;
+            }
+
+            if (DynamicVars[nameof(CurrentCount)].BaseValue > MaxCount)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = MaxCount;
+            }
+        }
+    }
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<SecretWeaponPower>(1), new IntVar(nameof(CurrentCount),3)];
+
+    public async Task OnCountdownZero(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<SecretWeaponPower>(choiceContext, Owner.Creature, DynamicVars["SecretWeaponPower"].BaseValue, Owner.Creature, this);
+    }
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await Countdown.OnCountdownPlay(choiceContext, cardPlay, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        this.UpgradeCountdown(-1);
+    }
+}
---
+++ TheThief/TheThiefCode/Powers/SecretWeaponPower.cs
@@ -0,0 +1,41 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Hooks;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class SecretWeaponPower : TheThiefPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    private decimal _amountToApply;
+
+    public override bool TryModifyPowerAmountReceived(PowerModel canonicalPower, Creature target, decimal amount, Creature? applier,
+        out decimal modifiedAmount)
+    {
+        if (target != Owner || canonicalPower.Type != PowerType.Buff || canonicalPower is not ITemporaryPower)
+        {
+            modifiedAmount = amount;
+            return false;
+        }
+
+        modifiedAmount = Math.Max(amount - this.Amount, 0);
+        _amountToApply = Math.Min(Amount, amount);
+        return true;
+    }
+
+    public override async Task AfterModifyingPowerAmountReceived(PowerModel power)
+    {
+        if (power is not ITemporaryPower tempPower)
+        {
+            return;
+        }
+        await PowerCmd.Apply(new ThrowingPlayerChoiceContext(), tempPower.InternallyAppliedPower, Owner, _amountToApply, power.Applier, null, true);
+    }
+
+}
