e4d1fdac5314ec4794ecc655a869d5ea7a40daaa
Pikcube <michaelds13@gmail.com>
2026-06-08 17:06:12 -0400

    Added Feedback Loop

---
+++ TheInventor/TheInventorCode/Cards/Uncommon/FeedbackLoop.cs
@@ -0,0 +1,25 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using Pikcube.Common.Extensions;
+using TheInventor.TheInventorCode.Gadgets;
+using TheInventor.TheInventorCode.Powers;
+
+namespace TheInventor.TheInventorCode.Cards.Uncommon;
+
+public class FeedbackLoop() : TheInventorCard(2, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    public override string GetScrapId => nameof(MagicDice);
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1), new EnergyVar(1)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await FeedbackLoopPower.ApplyAsync(choiceContext, Owner.Creature, DynamicVars.Cards.IntValue, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Cards.UpgradeValueBy(2);
+    }
+}
