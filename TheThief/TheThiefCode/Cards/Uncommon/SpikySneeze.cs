3ef856b6d50441d48dace91e7dba7eb483104516
Cory Zink <cz109918@ohio.edu>
2026-06-09 07:43:00 -0400

    Added Spiky Sneeze

---
+++ TheThief/TheThiefCode/Cards/Uncommon/SpikySneeze.cs
@@ -0,0 +1,26 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class SpikySneeze() : TheThiefCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+        [new DamageVar(9, ValueProp.Move), new IntVar("CountdownVar", 2)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+        CardModel? card = Owner.RunState.Rng.CombatCardSelection.NextItem(PileType.Hand.GetPile(Owner).Cards.Where(c => c is ICountdown));
+        if (card is ICountdown countdownCard)
+        {
+            await countdownCard.DecrementCount(2);
+        }
+
+    }
+}
