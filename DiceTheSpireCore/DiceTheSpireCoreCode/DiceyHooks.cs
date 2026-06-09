6b7abaaff84adfc9a0e77e868eae2e6319d070f6
Cory Zink <cz109918@ohio.edu>
2026-06-09 05:48:20 -0400

    Added Hooks and Listeners

---
+++ DiceTheSpireCore/DiceTheSpireCoreCode/DiceyHooks.cs
@@ -0,0 +1,22 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Listeners;
+using MegaCrit.Sts2.Core.Models;
+
+using RunState = MegaCrit.Sts2.Core.Runs.RunState;
+
+namespace DiceTheSpireCore.DiceTheSpireCoreCode;
+
+public static class DiceyHooks
+{
+    public delegate void AfterCardCountsDownHandler(RunState runState, CardModel countdownCard);
+
+    public static event AfterCardCountsDownHandler? AfterCardCountsDown;
+
+    internal static async Task OnAfterCardCountsDownAsync(RunState runState, CardModel countdownCard)
+    {
+        AfterCardCountsDown?.Invoke(runState, countdownCard);
+        foreach (IAfterCardCountsDownListener listener in runState.IterateHookListeners(countdownCard.Owner.Creature.CombatState).OfType<IAfterCardCountsDownListener>())
+        {
+            await listener.AfterCardCountsDownAsync(runState, countdownCard);
+        }
+    }
+}
