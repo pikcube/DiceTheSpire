eeaea7b8688400b8dd021d452cbbce2472abc34d
Cory Zink <cz109918@ohio.edu>
2026-06-09 19:20:56 -0400

    Fixed Spiky Sneeze not dealing damage

--- TheThief/TheThiefCode/Cards/Uncommon/SpikySneeze.cs
+++ TheThief/TheThiefCode/Cards/Uncommon/SpikySneeze.cs
@@ -1,5 +1,6 @@
 ﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
 using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -16,11 +17,19 @@ public class SpikySneeze() : TheThiefCard(1, CardType.Attack, CardRarity.Uncommo
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
+            .WithHitFx(VfxCmd.slashPath).Execute(choiceContext);
+
         CardModel? card = Owner.RunState.Rng.CombatCardSelection.NextItem(PileType.Hand.GetPile(Owner).Cards.Where(c => c is ICountdown));
         if (card is ICountdown countdownCard)
         {
             await countdownCard.DecrementCountAsync(2);
         }
+    }
 
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3);
     }
 }
