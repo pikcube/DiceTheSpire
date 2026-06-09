d18d13d86d78872c0986b2255dde897692f9508a
Cory Zink <cz109918@ohio.edu>
2026-06-09 05:50:22 -0400

    Added Wrecking Ball

---
+++ TheThief/TheThiefCode/Powers/WreckingBallPower.cs
@@ -0,0 +1,22 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Listeners;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Runs;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class WreckingBallPower : TheThiefPower, IAfterCardCountsDownListener
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public async Task AfterCardCountsDownAsync(RunState runState, CardModel countdownCard)
+    {
+        await CreatureCmd.Damage(new ThrowingPlayerChoiceContext(), CombatState.Enemies, Amount,
+            ValueProp.Unpowered | ValueProp.Unblockable, Owner, countdownCard);
+    }
+}
