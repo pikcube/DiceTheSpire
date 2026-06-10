5f45bf687bed96b7dc06402e77c02de4eaf47efc
Cory Zink <cz109918@ohio.edu>
2026-06-09 22:22:39 -0400

    Spiky Sneeze is now a countdown, and doesn't decrement counts already at 0

--- TheThief/TheThiefCode/Cards/Uncommon/SpikySneeze.cs
+++ TheThief/TheThiefCode/Cards/Uncommon/SpikySneeze.cs
@@ -9,27 +9,68 @@ using MegaCrit.Sts2.Core.ValueProps;
 
 namespace TheThief.TheThiefCode.Cards.Uncommon;
 
-public class SpikySneeze() : TheThiefCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+public class SpikySneeze() : TheThiefCard(-1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy), ICountdown
 {
+    public int MaxCount
+    {
+        get;
+        set
+        {
+            int changeBy = value - field;
+            field = value;
+            CurrentCount += changeBy;
+        }
+    } = 2;
+
+    public int CurrentCount
+    {
+        get => DynamicVars[nameof(CurrentCount)].IntValue;
+        set
+        {
+            DynamicVars[nameof(CurrentCount)].BaseValue = value;
+            if (DynamicVars[nameof(CurrentCount)].BaseValue < 0)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = 0;
+            }
+
+            if (DynamicVars[nameof(CurrentCount)].BaseValue > MaxCount)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = MaxCount;
+            }
+        }
+    }
+
     protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new DamageVar(9, ValueProp.Move), new IntVar("CountdownVar", 2)];
+        [new DamageVar(9, ValueProp.Move), new IntVar("CountdownVar", 2), new IntVar("CurrentCount", 2)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await Countdown.OnCountdownPlay(choiceContext, cardPlay, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(3);
+    }
+
+    public async Task OnCountdownZero(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         ArgumentNullException.ThrowIfNull(cardPlay.Target);
 
         await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
             .WithHitFx(VfxCmd.slashPath).Execute(choiceContext);
 
-        CardModel? card = Owner.RunState.Rng.CombatCardSelection.NextItem(PileType.Hand.GetPile(Owner).Cards.Where(c => c is ICountdown));
+        CardModel? card = Owner.RunState.Rng.CombatCardSelection.NextItem(PileType.Hand.GetPile(Owner).Cards.Where(c =>
+        {
+            if (c is ICountdown count)
+            {
+                return count.CurrentCount > 0;
+            }
+            return false;
+        }));
         if (card is ICountdown countdownCard)
         {
             await countdownCard.DecrementCountAsync(2);
         }
-    }
-
-    protected override void OnUpgrade()
-    {
-        DynamicVars.Damage.UpgradeValueBy(3);
     }
 }
