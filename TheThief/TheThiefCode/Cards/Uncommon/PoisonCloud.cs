2add1272ea4d24f83763c1d1b60be42f4758dc57
Cory Zink <cz109918@ohio.edu>
2026-06-10 00:20:39 -0400

    added Poison Cloud

---
+++ TheThief/TheThiefCode/Cards/Uncommon/PoisonCloud.cs
@@ -0,0 +1,28 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class PoisonCloud() : TheThiefCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PoisonPower>(6)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        await PowerCmd.Apply<PoisonPower>(choiceContext, CombatState.Enemies, DynamicVars.Poison.BaseValue,
+            Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Poison.UpgradeValueBy(3);
+    }
+}
