5626afa5db5042ae7e1bec152c26cfba93d8be00
Corey <coreySegiethMail@gmail.com>
2026-06-10 01:13:48 -0400

    Added Last Stand

--- DiceTheSpireCore/DiceTheSpireCoreCode/Powers/TargetShieldPower.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Powers/TargetShieldPower.cs
@@ -1,6 +1,7 @@
 ﻿using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
@@ -18,11 +19,10 @@ namespace DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
         public override PowerStackType StackType => PowerStackType.Counter;
 
         //AfterSideTurnStart
-        public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+        public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player) //AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
         {
             TargetShieldPower targetShieldPower = this;
-            if (!participants.Contains(targetShieldPower.Owner))
-                return;
+            //if (!participants.Contains(targetShieldPower.Owner)) return;
             targetShieldPower.Flash(); 
             await PowerCmd.Apply<DexterityPower>(new ThrowingPlayerChoiceContext(), targetShieldPower.Owner, targetShieldPower.Amount, targetShieldPower.Owner, null);
 
