2ab5b59880994c556447bba86b6f770acac9d7cf
Cory Zink <cz109918@ohio.edu>
2026-06-10 18:29:05 -0400

    Added Poison Crown

---
+++ TheThief/TheThiefCode/Cards/Uncommon/PoisonCrown.cs
@@ -0,0 +1,28 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheThief.TheThiefCode.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class PoisonCrown() : TheThiefCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PoisonPower>(1)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<PoisonCrownPower>(choiceContext, Owner.Creature, DynamicVars.Poison.BaseValue,
+            Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Poison.UpgradeValueBy(1);
+    }
+}
---
+++ TheThief/TheThiefCode/Powers/PoisonCrownPower.cs
@@ -0,0 +1,25 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class PoisonCrownPower : TheThiefPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task AfterCardGeneratedForCombat(CardModel card, Player? creator)
+    {
+        if (creator is null || creator != Owner.Player || card is not Pip)
+        {
+            return;
+        }
+        await PowerCmd.Apply<PoisonPower>(new ThrowingPlayerChoiceContext(), CombatState.Enemies, Amount, Owner, null);
+    }
+}
