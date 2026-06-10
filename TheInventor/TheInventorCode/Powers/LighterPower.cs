11af5577077bda4642ac628019ae6d9cf388ae7f
Pikcube <michaelds13@gmail.com>
2026-06-10 17:20:16 -0400

    Added Lighter

---
+++ TheInventor/TheInventorCode/Powers/LighterPower.cs
@@ -0,0 +1,24 @@
+﻿using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheInventor.TheInventorCode.Powers;
+
+public class LighterPower : TheInventorPower
+{
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override decimal ModifyDamageMultiplicative(Creature? target, decimal amount, ValueProp props, Creature? dealer,
+        CardModel? cardSource)
+    {
+        if (dealer == Owner)
+        {
+            return 1 + Amount/100m;
+        }
+
+        return 1;
+    }
+}
