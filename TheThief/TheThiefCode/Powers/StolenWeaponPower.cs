0862777b874e9c4c09c58e5a98fb8c2f74e4ca51
Cory Zink <cz109918@ohio.edu>
2026-06-10 22:27:46 -0400

    Stolen Weapon now applies to any form of Strength loss, but only applies to Strength

--- TheThief/TheThiefCode/Powers/StolenWeaponPower.cs
+++ TheThief/TheThiefCode/Powers/StolenWeaponPower.cs
@@ -1,26 +1,36 @@
-﻿using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Creatures;
+﻿using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
-using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
-using Pikcube.Common.Utility;
+using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace TheThief.TheThiefCode.Powers;
 
-public class StolenWeaponPower : TheThiefPower, IAfterPowerRemovedListener
+public class StolenWeaponPower : TheThiefPower
 {
     public override PowerType Type => PowerType.Buff;
-
     public override PowerStackType StackType => PowerStackType.Counter;
 
-    public async Task AfterPowerRemovedAsync(PowerModel powerModel, Creature? oldOwner)
+    public override bool TryModifyPowerAmountReceived(PowerModel canonicalPower, Creature target, decimal amount, Creature? applier,
+        out decimal modifiedAmount)
     {
-        if (oldOwner != Owner || powerModel.Type != PowerType.Buff ||
-            powerModel is not ITemporaryPower tempPower)
+        if (target != Owner)
         {
-            return;
+            modifiedAmount = amount;
+            return false;
         }
-        await PowerCmd.Apply(new ThrowingPlayerChoiceContext(), tempPower.InternallyAppliedPower, Owner, Math.Min(Amount, powerModel.Amount), Owner, null);
 
+        var amountToReduceBy = Math.Min(Amount, Math.Abs(amount));
+        switch (canonicalPower)
+        {
+            case StrengthPower when canonicalPower.GetTypeForAmount(amount) == PowerType.Debuff:
+                modifiedAmount = amount + amountToReduceBy;
+                return true;
+            case TemporaryStrengthPower { Type: PowerType.Debuff }:
+                modifiedAmount = amount - amountToReduceBy;
+                return true;
+            default:
+                modifiedAmount = amount;
+                return false;
+        }
     }
 }
