4c0f8ea3ae98030978d43b10f864f9db8f9619cb
Cory Zink <cz109918@ohio.edu>
2026-06-11 07:32:33 -0400

    Added Rat

---
+++ TheThief/TheThiefCode/Cards/Uncommon/Rat.cs
@@ -0,0 +1,63 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class Rat() : TheThiefCard(-1, CardType.Skill, CardRarity.Uncommon, TargetType.AnyEnemy), ICountdown
+{
+    public int MaxCount
+    {
+        get;
+        set
+        {
+            int changeBy = value - field;
+            field = value;
+            CurrentCount += changeBy;
+        }
+    } = 2;
+
+    public int CurrentCount
+    {
+        get => DynamicVars[nameof(CurrentCount)].IntValue;
+        set
+        {
+            DynamicVars[nameof(CurrentCount)].BaseValue = value;
+            if (DynamicVars[nameof(CurrentCount)].BaseValue < 0)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = 0;
+            }
+
+            if (DynamicVars[nameof(CurrentCount)].BaseValue > MaxCount)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = MaxCount;
+            }
+        }
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1), new PowerVar<PoisonPower>(5), new IntVar("CurrentCount", 2)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await Countdown.OnCountdownPlay(choiceContext, cardPlay, this);
+    }
+
+    public async Task OnCountdownZero(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await PowerCmd.Apply<PoisonPower>(choiceContext, cardPlay.Target, DynamicVars.Poison.BaseValue, Owner.Creature,
+            this);
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Poison.UpgradeValueBy(1);
+        DynamicVars.Cards.UpgradeValueBy(1);
+    }
+}
