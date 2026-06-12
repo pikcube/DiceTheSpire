b5a1ad31448ad4cd2b6add3bb27f0fe1874b4f13
Corey <coreySegiethMail@gmail.com>
2026-06-11 21:34:42 -0400

    Added Candle. Its a bit buggy, but technically it is working... mostly... The burn isn't upgraded and the loop doesn't allow one reroll to resolve before beginning the next. I don't have time to fix it tonight. It is error free at least.

---
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/Candle.cs
@@ -0,0 +1,80 @@
+﻿using BaseLib.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode;
+using Godot;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Models.Characters;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.TestSupport;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon
+{
+
+public class Candle() : TheWarriorCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+
+        private int _testEnergyCostOverride = -1;
+
+        protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(2), new RepeatVar(3)];
+        protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Reroll)];
+
+        public int TestEnergyCostOverride
+        {
+            get => _testEnergyCostOverride;
+            set
+            {
+                TestMode.AssertOn();
+                AssertMutable();
+                _testEnergyCostOverride = value;
+            }
+        }
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            NCombatRoom.Instance?.PlaySplashVfx(Owner.Creature, new Godot.Color("6ec46f"));
+
+            for (int n = 0; n < DynamicVars.Repeat.IntValue; ++n)
+            {
+                CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_RANDOMIZE"), 0, DynamicVars.Cards.IntValue);
+                CardModel[] cards = [.. await CardSelectCmd.FromHand(choiceContext, Owner, cardSelectorPrefs, null, this)];
+                foreach (CardModel card in cards.Where(c => !c.EnergyCost.CostsX))
+                {
+                    if (card.EnergyCost.GetWithModifiers(CostModifiers.None) >= 0)
+                    {
+                        card.EnergyCost.SetThisTurnOrUntilPlayed(NextEnergyCost());
+                        NCard.FindOnTable(card)?.PlayRandomizeCostAnim();
+                    }
+                }
+                await Cmd.Wait(0.5f);
+ 
+            }
+            if(CombatState is null)
+            {
+                return;
+            }
+            Candle candle = this;
+            CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat((CardModel)CombatState.CreateCard<Burn>(Owner), PileType.Draw, Owner));
+            await Cmd.Wait(0.5f);
+        }
+
+        private int NextEnergyCost()
+        {
+            return TestEnergyCostOverride >= 0 ? TestEnergyCostOverride : Owner.RunState.Rng.CombatEnergyCosts.NextInt(4);
+        }
+
+        protected override void OnUpgrade()
+        {
+            DynamicVars.Repeat.UpgradeValueBy(1);
+        }
+    }
+}
+
