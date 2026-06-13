22c9d300910be4e2b0f3b14de12ccd7f23230f11
Pikcube <michaelds13@gmail.com>
2026-06-13 19:40:22 -0400

    Remote Control now exhausts

--- TheInventor/TheInventorCode/Cards/Common/RemoteControl.cs
+++ TheInventor/TheInventorCode/Cards/Common/RemoteControl.cs
@@ -3,7 +3,6 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
-using Pikcube.Common.Keywords;
 using TheInventor.TheInventorCode.Gadgets;
 using TheInventor.TheInventorCode.Relics;
 
@@ -14,7 +13,7 @@ public class RemoteControl() : TheInventorCard(1, CardType.Attack, CardRarity.Co
 {
     public override string GetScrapId => nameof(Efficiency);
     protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(7, DamageProps.card)];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [BlinkModel.Blink];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
