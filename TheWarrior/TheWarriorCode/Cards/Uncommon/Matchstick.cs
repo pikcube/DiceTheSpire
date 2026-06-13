e669d9b0045c91d9e3b2560b066a65f850b487d8
Corey <coreySegiethMail@gmail.com>
2026-06-12 21:06:53 -0400

    Added Matchstick

---
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/Matchstick.cs
@@ -0,0 +1,33 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using Pikcube.Common.Keywords;
+using Pikcube.Common.Utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon
+{
+
+public class Matchstick() : TheWarriorCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+    {
+        protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3), new EnergyVar(1)];
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            await PlayerCmd.GainEnergy(DynamicVars.Energy.IntValue, Owner);
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.IntValue, Owner);
+        }
+
+        protected override void OnUpgrade()
+        {
+            DynamicVars.Cards.UpgradeValueBy(1);
+        }
+
+    }
+}
