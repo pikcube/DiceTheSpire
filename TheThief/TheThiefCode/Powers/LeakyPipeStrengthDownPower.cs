398c47faa473724c116e8ea6bf886261acb176d0
Cory Zink <cz109918@ohio.edu>
2026-06-14 15:04:22 -0400

    Added Leaky Pipe

---
+++ TheThief/TheThiefCode/Powers/LeakyPipeStrengthDownPower.cs
@@ -0,0 +1,12 @@
+﻿using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheThief.TheThiefCode.Cards.Rare;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class LeakyPipeStrengthDownPower : TemporaryStrengthPower
+{
+    public override AbstractModel OriginModel => ModelDb.Card<LeakyPipe>();
+
+    protected override bool IsPositive => false;
+}
