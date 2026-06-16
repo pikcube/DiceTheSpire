41b328640b3c5f6eefd3ef62f68259e9917f8971
Corey <coreySegiethMail@gmail.com>
2026-06-15 21:48:40 -0400

    Added Sword, Added Roll Again, Reworked Combat Roll, and adjusted the Warrior's starting deck.

--- TheWarrior/TheWarriorCode/Cards/Basic/CombatRoll.cs
+++ TheWarrior/TheWarriorCode/Cards/Basic/CombatRoll.cs
@@ -5,32 +5,34 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using TheWarrior.TheWarriorCode.Cards.Uncommon;
 
 namespace TheWarrior.TheWarriorCode.Cards.Basic;
 
 
-public class CombatRoll() : TheWarriorCard(0, CardType.Skill, CardRarity.Basic, TargetType.Self)
+public class CombatRoll() : TheWarriorCard(1, CardType.Skill, CardRarity.Basic, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(2)];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
+
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-
-        CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_DISCARD"), 0, DynamicVars.Cards.IntValue);
-        CardModel[] cards = [.. await CardSelectCmd.FromHand(choiceContext, Owner, cardSelectorPrefs, null, this)];
-        foreach (CardModel card in cards)
-        {
-            await CardCmd.Discard(choiceContext, card);
-        }
-
-        if (cards.Length == 0)
+        if (CombatState is null)
         {
             return;
         }
 
-        await CardPileCmd.Draw(choiceContext, cards.Length, Owner);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<RollAgain>(Owner), PileType.Hand, Owner));
+        await Cmd.Wait(0.25f);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<RollAgain>(Owner), PileType.Hand, Owner));
+        await Cmd.Wait(0.25f);
+        CardCmd.PreviewCardPileAdd(await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<RollAgain>(Owner), PileType.Hand, Owner));
+        await Cmd.Wait(0.25f);
+
     }
 
-    protected override void OnUpgrade() => DynamicVars.Cards.UpgradeValueBy(1);
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
 
 }
---
+++ TheWarrior/TheWarriorCode/Cards/Token/RollAgain.cs
@@ -0,0 +1,71 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode;
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.TestSupport;
+
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon
+{
+
+
+public class RollAgain() : TheWarriorCard(0, CardType.Skill, CardRarity.Token, TargetType.Self)
+    {
+
+        private int _testEnergyCostOverride = -1;
+        public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Ethereal];
+        protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1)];
+        protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Reroll)];
+
+        public int TestEnergyCostOverride
+        {
+            get => _testEnergyCostOverride;
+            set
+            {
+                TestMode.AssertOn();
+                AssertMutable();
+                _testEnergyCostOverride = value;
+            }
+        }
+
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            NCombatRoom.Instance?.PlaySplashVfx(Owner.Creature, new Color("6ec46f"));
+
+                CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_RANDOMIZE"), DynamicVars.Cards.IntValue, DynamicVars.Cards.IntValue);
+                CardModel[] cards = [.. await CardSelectCmd.FromHand(choiceContext, Owner, cardSelectorPrefs, null, this)];
+                foreach (CardModel card in cards.Where(c => !c.EnergyCost.CostsX))
+                {
+                    if (card.EnergyCost.GetWithModifiers(CostModifiers.None) >= 0)
+                    {
+                    card.EnergyCost.SetThisTurnOrUntilPlayed(NextEnergyCost());
+                    await Cmd.Wait(0.5f);
+                    NCard.FindOnTable(card)?.PlayRandomizeCostAnim();
+                    }
+                }
+
+        }
+        
+
+        private int NextEnergyCost()
+        {
+            return TestEnergyCostOverride >= 0 ? TestEnergyCostOverride : Owner.RunState.Rng.CombatEnergyCosts.NextInt(4);
+        }
+
+        protected override void OnUpgrade()
+        {
+            RemoveKeyword(CardKeyword.Ethereal);
+            AddKeyword(CardKeyword.Retain);
+        }
+    }
+}
+
