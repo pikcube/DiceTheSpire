736e7b040bc9cd85168df47924dec1436c3f0f9b
Cory Zink <cz109918@ohio.edu>
2026-06-17 17:06:25 -0400

    Added Cactus Shield and Cactus Spear

---
+++ TheThief/TheThiefCode/Powers/CactusShieldPower.cs
@@ -0,0 +1,11 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheThief.TheThiefCode.Cards.Common;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class CactusShieldPower: TemporaryThornsPower
+{
+    public override AbstractModel OriginModel => ModelDb.Card<CactusShield>();
+}
