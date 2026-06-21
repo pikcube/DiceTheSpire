aea3ffded114ccd2b3b976a2ca3afa85c2ffc2d7
Cory Zink <cz109918@ohio.edu>
2026-06-20 21:57:17 -0400

    Parrying Dagger gives Reduce instead of Block

--- TheThief/TheThiefCode/Cards/Common/ParryingDagger.cs
+++ TheThief/TheThiefCode/Cards/Common/ParryingDagger.cs
@@ -1,28 +1,30 @@
-﻿using MegaCrit.Sts2.Core.Commands;
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
+using TheThief.TheThiefCode.Powers;
 
 namespace TheThief.TheThiefCode.Cards.Common;
 
 public class ParryingDagger(): TheThiefCard(1, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars =>
-        [new DamageVar(5, ValueProp.Move), new BlockVar(5, ValueProp.Move)];
+        [new DamageVar(6, ValueProp.Move), new PowerVar<ReducePower>(1)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         ArgumentNullException.ThrowIfNull(cardPlay.Target);
 
-        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+        await PowerCmd.Apply<ParryingDaggerPower>(choiceContext, Owner.Creature, DynamicVars["ReducePower"].BaseValue,
+            Owner.Creature, this);
         await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this).Targeting(cardPlay.Target)
             .Execute(choiceContext);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Block.UpgradeValueBy(2);
-        DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars.Damage.UpgradeValueBy(3);
     }
 }
---
+++ TheThief/TheThiefCode/Powers/ParryingDaggerPower.cs
@@ -0,0 +1,10 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using MegaCrit.Sts2.Core.Models;
+using TheThief.TheThiefCode.Cards.Common;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class ParryingDaggerPower : TemporaryReducePower
+{
+    public override AbstractModel OriginModel => ModelDb.Card<ParryingDagger>();
+}
