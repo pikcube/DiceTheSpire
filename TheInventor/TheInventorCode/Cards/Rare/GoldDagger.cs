43169229fd17cd2c9cc73babe0075c644af9e1ee
Pikcube <michaelds13@gmail.com>
2026-06-22 00:23:04 -0400

    Added Golden Dagger

---
+++ TheInventor/TheInventorCode/Cards/Rare/GoldDagger.cs
@@ -0,0 +1,32 @@
+﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheInventor.TheInventorCode.Gadgets;
+using TheInventor.TheInventorCode.Keywords;
+
+namespace TheInventor.TheInventorCode.Cards.Rare;
+
+public class GoldDagger() : TheInventorCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+{
+    public override string GetScrapId => nameof(Harvest);
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(16, DamageProps.card)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .Targeting(cardPlay.Target)
+            .WithHitFx(VfxCmd.slashPath)
+            .Execute(choiceContext);
+    }
+
+    protected override void OnUpgrade()
+    {
+        AddKeyword(ScrapKeyword.Scrap);
+    }
+}
