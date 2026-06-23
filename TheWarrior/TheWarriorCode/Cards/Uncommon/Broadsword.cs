82ed07b966b98e00ca9d6f1142b785f6e65ec5c1
Pikcube <michaelds13@gmail.com>
2026-06-23 10:21:52 -0400

    Removed unused usings

--- TheWarrior/TheWarriorCode/Cards/Uncommon/Broadsword.cs
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/Broadsword.cs
@@ -7,13 +7,7 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Nodes.Cards;
 using MegaCrit.Sts2.Core.ValueProps;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TheWarrior.TheWarriorCode.Cards.Uncommon
 {
