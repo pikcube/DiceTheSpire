a4c1076667cb0815285f7d2b9856704c0d28615a
Pikcube <michaelds13@gmail.com>
2026-06-25 12:23:37 -0400

    Removed unused usings

--- TheWarrior/TheWarriorCode/Cards/Rare/CrescentMoonBlade.cs
+++ TheWarrior/TheWarriorCode/Cards/Rare/CrescentMoonBlade.cs
@@ -1,17 +1,8 @@
 ﻿using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Nodes.Combat;
 using MegaCrit.Sts2.Core.ValueProps;
-using Pikcube.Common.Keywords;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TheWarrior.TheWarriorCode.Cards.Rare
 {
@@ -26,6 +17,8 @@ namespace TheWarrior.TheWarriorCode.Cards.Rare
             {
                 return;
             }
+
+            ArgumentNullException.ThrowIfNull(cardPlay.Target);
 
             int xValue = cardPlay.Card.ResolveEnergyXValue();
 
