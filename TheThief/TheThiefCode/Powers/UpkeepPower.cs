46500e59673fed7cf0543fd85a1d385b7c3434ba
Cory Zink <cz109918@ohio.edu>
2026-06-27 16:52:06 -0400

    Added Upkeep (new Dusty Tome card)

---
+++ TheThief/TheThiefCode/Powers/UpkeepPower.cs
@@ -0,0 +1,41 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Listeners;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class UpkeepPower : TheThiefPower, IModifyPipOnPlayListener
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
+    Player IModifyPipOnPlayListener.Owner => Owner.Player ?? throw new InvalidOperationException();
+    public IEnumerable<IHoverTip> PipHoverTips => [HoverTipFactory.FromPower<PoisonPower>(), HoverTipFactory.ForEnergy(this)];
+
+    public LocString PipDescription
+    {
+        get
+        {
+            LocString l = new LocString("powers", Id.Entry + ".pipDescription").WithDynamicVars(DynamicVars);
+            l.Add("Amount", Amount);
+            return l;
+        }
+    }
+    public async Task ModifyPipOnPlayAsync(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Owner != Owner.Player)
+        {
+            return;
+        }
+        await PlayerCmd.GainEnergy(1, Owner.Player);
+    }
+}
