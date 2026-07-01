214fd2bafcd27253f8a73e3912c24b5de5561676
Cory Zink <cz109918@ohio.edu>
2026-06-30 23:27:07 -0400

    Bench Press Power is now prefixed properly

--- TheThief/TheThiefCode/Powers/BenchPressPower.cs
+++ TheThief/TheThiefCode/Powers/BenchPressPower.cs
@@ -1,10 +1,11 @@
-﻿using MegaCrit.Sts2.Core.Models;
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 using TheThief.TheThiefCode.Cards.Common;
 
 namespace TheThief.TheThiefCode.Powers;
 
-public class BenchPressPower : TemporaryStrengthPower
+public class BenchPressPower : TemporaryStrengthPower, ICustomModel
 {
     public override AbstractModel OriginModel => ModelDb.Card<BenchPress>();
 }
