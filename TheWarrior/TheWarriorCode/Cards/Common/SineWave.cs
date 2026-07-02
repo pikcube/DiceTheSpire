b6eb92b31e8a460385c0387a916daf1290b84e8e
Corey <coreySegiethMail@gmail.com>
2026-07-02 00:39:44 -0400

    Reworked Sine wave

--- TheWarrior/TheWarriorCode/Cards/Common/SineWave.cs
+++ TheWarrior/TheWarriorCode/Cards/Common/SineWave.cs
@@ -1,32 +1,72 @@
 ﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Nodes.Cards;
+using MegaCrit.Sts2.Core.Nodes.CommonUi;
+using static Godot.HttpRequest;
 
 namespace TheWarrior.TheWarriorCode.Cards.Common
 {
 
-    public class SineWave() : TheWarriorCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    public class SineWave() : TheWarriorCard(0, CardType.Skill, CardRarity.Common, TargetType.Self)
     {
         public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
-        protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Bump>(), HoverTipFactory.FromCard<Nudge>()];
 
         protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
         {
-            if (CombatState is null)
+            if (CombatState is null || Owner is null || Owner.PlayerCombatState is null)
             {
                 return;
             }
-            await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<Bump>(Owner), PileType.Hand, Owner);
-            await Cmd.Wait(0.25f);
-            await CardPileCmd.AddGeneratedCardToCombat(CombatState.CreateCard<Nudge>(Owner), PileType.Hand, Owner);
-            await Cmd.Wait(0.25f);
+            //List<CardModel> drawPile = [.. Owner.PlayerCombatState.DrawPile.Cards];
+            //List<CardModel> handPile = [.. Owner.PlayerCombatState.Hand.Cards];
+            if (!IsUpgraded)
+                {
+                    CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_BUMP"), 1, 1);
+                    IEnumerable<CardModel> results = await CardSelectCmd.FromCombatPile(choiceContext, PileType.Draw.GetPile(Owner), Owner, cardSelectorPrefs);
+                    foreach (CardModel card in results)
+                    {
+                        await card.BumpAsync(choiceContext);
+                    }
+                } else
+                {
+                    CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_BUMP"), 1, 1);
+                    IEnumerable<CardModel> results = await CardSelectCmd.FromCombatPile(choiceContext, PileType.Hand.GetPile(Owner), Owner, cardSelectorPrefs);
+                    foreach (CardModel card in results)
+                    {
+                        await card.BumpAsync(choiceContext);
+                    }
+                }
+
+                if (!IsUpgraded)
+                {
+                    CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_NUDGE"), 1, 1);
+                    IEnumerable<CardModel> results = await CardSelectCmd.FromCombatPile(choiceContext, PileType.Hand.GetPile(Owner), Owner, cardSelectorPrefs);
+                    foreach (CardModel card in results)
+                    {
+                        await card.NudgeAsync(choiceContext);
+                    }
+                } else
+                {
+                    CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_NUDGE"), 1, 1);
+                    IEnumerable<CardModel> results = await CardSelectCmd.FromCombatPile(choiceContext, PileType.Draw.GetPile(Owner), Owner, cardSelectorPrefs);
+                    foreach (CardModel card in results)
+                    {
+                        await card.NudgeAsync(choiceContext);
+                    }
+                }
         }
 
         protected override void OnUpgrade()
         {
-            EnergyCost.UpgradeBy(-1);
+            base.OnUpgrade();
         }
 
     }
