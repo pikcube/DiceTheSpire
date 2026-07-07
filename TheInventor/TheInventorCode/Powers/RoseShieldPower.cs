ce51c42b8af6cf2d5f1ab953e66359ddcc854f93
Pikcube <michaelds13@gmail.com>
2026-07-07 17:16:12 -0400

    Tweaked Change Machine

--- TheInventor/TheInventorCode/Powers/RoseShieldPower.cs
+++ TheInventor/TheInventorCode/Powers/RoseShieldPower.cs
@@ -4,6 +4,7 @@ using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.Models.Powers;
