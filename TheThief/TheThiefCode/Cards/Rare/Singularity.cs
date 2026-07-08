c8f43ee3df66155790815be8a23e63a4655e77b4
Cory Zink <cz109918@ohio.edu>
2026-07-08 05:50:58 -0400

    Singularity reworked. Is now a rare Power.

---
+++ TheThief/TheThiefCode/Cards/Rare/Singularity.cs
@@ -0,0 +1,33 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using TheThief.TheThiefCode.Cards.Token;
+using TheThief.TheThiefCode.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Rare;
+
+public class Singularity() : TheThiefCard(1, CardType.Power, CardRarity.Rare, TargetType.Self)
+{
+    
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        await PowerCmd.Apply<SingularityPower>(choiceContext, Owner.Creature, 1, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        AddKeyword(CardKeyword.Innate);
+    }
+}
