a1d1d09aeec717c5a4e4af25226ae2817a1abac8
Corey <coreySegiethMail@gmail.com>
2026-07-10 00:26:43 -0400

    Added Power Crystal. PowerCrystalPower is not yet made.

---
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/PowerCrystal.cs
@@ -0,0 +1,30 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon
+{
+
+    public class PowerCrystal() : TheWarriorCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    {
+        //protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PowerCrystalPower>(1M)];
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+            //await PowerCmd.Apply<PowerCrystalPower>(choiceContext, Owner.Creature, 1M, Owner.Creature, this);
+        }
+
+        protected override void OnUpgrade()
+        {
+            AddKeyword(CardKeyword.Innate);
+        }
+    }
+}
