66be6b2085e04b091025b25aaa1e16237d16b760
Corey <coreySegiethMail@gmail.com>
2026-07-10 00:01:52 -0400

    Repush

---
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/SpareThree.cs
@@ -0,0 +1,36 @@
+﻿using BaseLib.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Commands;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Extensions;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon;
+public class SpareThree() : TheWarriorCard(0, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(3), new PowerVar<NoDrawPower>(1M)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PlayerCmd.GainEnergy(DynamicVars.Energy.IntValue, Owner);
+        await PowerCmd.Apply<NoDrawPower>(choiceContext, Owner.Creature, DynamicVars.Power<NoDrawPower>().BaseValue, Owner.Creature, this);
+    }
+
+    public override string CustomPortraitPath => IsUpgraded ? "spare_four.png".BigCardImagePath() : "spare_three.png".BigCardImagePath();
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Energy.UpgradeValueBy(1);
+    }
+}
+
