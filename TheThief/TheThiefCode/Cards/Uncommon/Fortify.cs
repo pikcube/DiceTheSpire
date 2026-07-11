ce7c93dd9090d0b35d7b3c120cd159ea4441a980
Cory Zink <cz109918@ohio.edu>
2026-07-10 22:44:56 -0400

    Added Fortify

---
+++ TheThief/TheThiefCode/Cards/Uncommon/Fortify.cs
@@ -0,0 +1,33 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheThief.TheThiefCode.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class Fortify() : TheThiefCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(3, ValueProp.Move)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        await PowerCmd.Apply<FortifyPower>(choiceContext, Owner.Creature, DynamicVars.Block.BaseValue,
+            Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Block.UpgradeValueBy(1);
+    }
+}
---
+++ TheThief/TheThiefCode/Powers/FortifyPower.cs
@@ -0,0 +1,31 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using TheThief.TheThiefCode.Cards.Uncommon;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class FortifyPower : TheThiefPower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side != Owner.Side || Owner.Player?.PlayerCombatState is null)
+        {
+            return;
+        }
+
+        foreach (CardModel card in Owner.Player.PlayerCombatState.Hand.Cards.Where((c, _) => c is Pip))
+        {
+            await CreatureCmd.GainBlock(Owner, Amount, ValueProp.Move, null);
+        }
+    }
+}
