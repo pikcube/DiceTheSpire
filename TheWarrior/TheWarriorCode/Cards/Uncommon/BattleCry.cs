775d9fae3dfbf7561454a7f660ec099d449f0007
Corey <coreySegiethMail@gmail.com>
2026-07-12 16:25:31 -0400

    Battle Cry now uses Range Keyword

--- TheWarrior/TheWarriorCode/Cards/Uncommon/BattleCry.cs
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/BattleCry.cs
@@ -1,16 +1,21 @@
 ﻿using BaseLib.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Utilities;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using TheWarrior.TheWarriorCode.Extensions;
 
 namespace TheWarrior.TheWarriorCode.Cards.Uncommon;
 
-public class BattleCry() : TheWarriorCard(3, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies)
+public class BattleCry() : TheWarriorCard(3, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies), IRangeCard
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<VigorPower>(8M), new PowerVar<VulnerablePower>(3)];
-
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [BetterStaticHoverTips.RangeHoverTip(this), HoverTipFactory.FromPower<VigorPower>(DynamicVars.Power<VigorPower>().IntValue), HoverTipFactory.FromPower<VulnerablePower>(DynamicVars.Power<VulnerablePower>().IntValue)];
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         if (RunState is null || CombatState is null)
@@ -21,6 +26,8 @@ public class BattleCry() : TheWarriorCard(3, CardType.Skill, CardRarity.Uncommon
         await PowerCmd.Apply<VigorPower>(choiceContext, Owner.Creature, DynamicVars.Power<VigorPower>().IntValue, Owner.Creature, cardPlay.Card);
     }
 
+    public int MinimumCost => 0;
+    public int MaximumCost => 2;
     protected override void OnUpgrade()
     {
         DynamicVars.Power<VigorPower>().UpgradeValueBy(2);
