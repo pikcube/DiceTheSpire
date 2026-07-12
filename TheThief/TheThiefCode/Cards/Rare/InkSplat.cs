e0bd39e27b846173c8efa03c820f33233789e56c
Cory Zink <cz109918@ohio.edu>
2026-07-11 20:26:09 -0400

    Added Ink Splat

---
+++ TheThief/TheThiefCode/Cards/Rare/InkSplat.cs
@@ -0,0 +1,39 @@
+﻿using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Enchantments;
+
+namespace TheThief.TheThiefCode.Cards.Rare;
+
+public class InkSplat() : TheThiefCard(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+{
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [..HoverTipFactory.FromEnchantment<Inky>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        CardSelectorPrefs prefs = new(CardSelectorPrefs.EnchantSelectionPrompt, 1);
+        IEnumerable<CardModel> cards = await CardSelectCmd.FromHand(choiceContext, Owner, prefs,
+            c => c.Type == CardType.Attack && ModelDb.Enchantment<Inky>().CanEnchant(c), this);
+        CardModel? card = cards.FirstOrDefault();
+        if (card is null)
+        {
+            return;
+        }
+
+        CardCmd.Enchant<Inky>(card, 1);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
+}
