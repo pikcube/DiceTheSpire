9125a98913c2dd31ced3ef67eee68eedd454688c
Pikcube <michaelds13@gmail.com>
2026-07-13 17:18:45 -0400

    Added every Inventor Relic (most of them don't work yet)

---
+++ TheInventor/TheInventorCode/Relics/Toolbelt.cs
@@ -0,0 +1,8 @@
+﻿using MegaCrit.Sts2.Core.Entities.Relics;
+
+namespace TheInventor.TheInventorCode.Relics;
+
+public class Toolbelt : TheInventorRelic
+{
+    public override RelicRarity Rarity => RelicRarity.Common;
+}
