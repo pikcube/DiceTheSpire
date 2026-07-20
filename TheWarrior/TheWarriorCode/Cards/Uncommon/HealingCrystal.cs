93ed56f2e093d55550459373442a07ae3d4514d8
Pikcube <michaelds13@gmail.com>
2026-07-20 14:58:50 -0400

    Fixed namespace

--- TheWarrior/TheWarriorCode/Cards/Uncommon/HealingCrystal.cs
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/HealingCrystal.cs
@@ -3,30 +3,28 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
-namespace TheWarrior.TheWarriorCode.Cards.Uncommon
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon;
+
+public class HealingCrystal() : TheWarriorCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
-    public class HealingCrystal() : TheWarriorCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self)
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(3), new HealVar(7M)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal];
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(3), new HealVar(7M)];
-        public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Ethereal];
-        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        if (Owner.PlayerCombatState is null)
         {
-            if (Owner.PlayerCombatState is null)
-            {
-                return;
-            }
-            if (IsPlayable)
-            {
-                await CreatureCmd.Heal(Owner.Creature, DynamicVars.Heal.IntValue);
-            }
+            return;
         }
-        protected override bool IsPlayable => Owner.PlayerCombatState?.Hand.Cards.Sum(c => c.EnergyCost.GetAmountToSpend()) <= DynamicVars.Energy.IntValue;
-        protected override bool ShouldGlowGoldInternal => IsPlayable;
-        protected override void OnUpgrade()
+        if (IsPlayable)
         {
-            DynamicVars.Heal.UpgradeValueBy(2);
-            DynamicVars.Energy.UpgradeValueBy(1);
+            await CreatureCmd.Heal(Owner.Creature, DynamicVars.Heal.IntValue);
         }
     }
-    
+    protected override bool IsPlayable => Owner.PlayerCombatState?.Hand.Cards.Sum(c => c.EnergyCost.GetAmountToSpend()) <= DynamicVars.Energy.IntValue;
+    protected override bool ShouldGlowGoldInternal => IsPlayable;
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Heal.UpgradeValueBy(2);
+        DynamicVars.Energy.UpgradeValueBy(1);
+    }
 }
