d185313a684928f6c591b153f730b4b8c016e9b3
Pikcube <michaelds13@gmail.com>
2026-07-22 00:05:06 -0400

    Reworked Magnifying Glass

--- TheInventor/TheInventorCode/Relics/ToastedMarshmallow.cs
+++ TheInventor/TheInventorCode/Relics/ToastedMarshmallow.cs
@@ -1,10 +1,9 @@
-﻿using MegaCrit.Sts2.Core.Combat;
-using MegaCrit.Sts2.Core.Entities.Creatures;
+﻿using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Rooms;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace TheInventor.TheInventorCode.Relics;
 
@@ -13,48 +12,20 @@ public class ToastedMarshmallow : TheInventorRelic
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new EnergyVar(1)];
     public override RelicRarity Rarity => RelicRarity.Uncommon;
-    private int ModifyNextDraw { get; set; }
 
-    public override Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    public override async Task AfterAutoPostPlayPhaseEntered(PlayerChoiceContext choiceContext, Player player)
     {
-        if (side != CombatSide.Player || !(Owner.PlayerCombatState?.Energy > 0))
+        if (player != Owner)
         {
-            return Task.CompletedTask;
+            return;
         }
 
-        ModifyNextDraw = 1;
-        Status = RelicStatus.Active;
+        int count = player.PlayerCombatState?.Energy ?? 0;
 
-        return Task.CompletedTask;
-    }
-
-    public override decimal ModifyHandDraw(Player player, decimal count)
-    {
-        if (Owner != player)
+        for (int n = 0; n < count; ++n)
         {
-            return count;
+            await CreatureCmd.GainBlock(player.Creature, 2, BlockProps.nonCardUnpowered, null);
         }
-
-        return count + ModifyNextDraw;
-    }
-
-    public override Task AfterModifyingHandDraw()
-    {
-        Status = RelicStatus.Normal;
-        if (ModifyNextDraw <= 0)
-        {
-            return Task.CompletedTask;
-        }
-
         Flash();
-        ModifyNextDraw = 0;
-        return Task.CompletedTask;
-    }
-
-    public override Task AfterCombatEnd(CombatRoom room)
-    {
-        ModifyNextDraw = 0;
-        Status = RelicStatus.Normal;
-        return Task.CompletedTask;
     }
 }
