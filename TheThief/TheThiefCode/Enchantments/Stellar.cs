9fe375cbff2ff26a97dd47ff722fa6d72e62377e
Pikcube <michaelds13@gmail.com>
2026-07-23 00:38:06 -0400

    Fixed Stellar

--- TheThief/TheThiefCode/Enchantments/Stellar.cs
+++ TheThief/TheThiefCode/Enchantments/Stellar.cs
@@ -5,6 +5,7 @@ using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Cards;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace TheThief.TheThiefCode.Enchantments;
