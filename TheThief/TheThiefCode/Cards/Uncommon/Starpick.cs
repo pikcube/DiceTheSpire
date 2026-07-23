3a01a567caee0e9948e341160582bba2f145f0e7
Cory Zink <cz109918@ohio.edu>
2026-07-23 00:28:48 -0400

    Added Stellar and added Starpick image (Starpick card locks game when played. WIP)

--- TheThief/TheThiefCode/Cards/Uncommon/Starpick.cs
+++ TheThief/TheThiefCode/Cards/Uncommon/Starpick.cs
@@ -1,8 +1,11 @@
 ﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using TheThief.TheThiefCode.Enchantments;
 
 namespace TheThief.TheThiefCode.Cards.Uncommon;
 
@@ -10,7 +13,7 @@ public class Starpick() : TheThiefCard(1, CardType.Skill, CardRarity.Uncommon, T
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(2)];
 
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>(), ..HoverTipFactory.FromEnchantment<Stellar>()];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
@@ -19,7 +22,10 @@ public class Starpick() : TheThiefCard(1, CardType.Skill, CardRarity.Uncommon, T
             return;
         }
 
-
+        foreach (CardModel card in await Pip.CreateInHandAsync(Owner, DynamicVars.Cards.IntValue, CombatState))
+        {
+            CardCmd.Enchant<Stellar>(card, 1);
+        }
     }
 
     protected override void OnUpgrade()
---
+++ TheThief/TheThiefCode/Enchantments/Stellar.cs
@@ -0,0 +1,23 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace TheThief.TheThiefCode.Enchantments;
+
+public class Stellar : CustomEnchantmentModel
+{
+    public override bool HasExtraCardText => true;
+    public override bool ShowAmount => false;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new StarsVar(1)];
+
+    public override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay? cardPlay)
+    {
+        if (cardPlay is null)
+        {
+            return;
+        }
+        await PlayerCmd.GainStars(DynamicVars.Stars.BaseValue, cardPlay.Player);
+    }
+}
