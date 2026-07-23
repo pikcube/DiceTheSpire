cd4ce7dc5456253a6fe1c580d4c4c3389ede505f
Corey <coreySegiethMail@gmail.com>
2026-07-22 21:23:25 -0400

    Added Kite Shield

---
+++ TheWarrior/TheWarriorCode/Cards/Rare/KiteShield.cs
@@ -0,0 +1,32 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.ValueProps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheWarrior.TheWarriorCode.Cards.Rare;
+public class KiteShield() : TheWarriorCard(6, CardType.Skill, CardRarity.Rare, TargetType.Self), IRangeCard
+{
+    public override bool GainsBlock => true;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new BlockVar(40, BlockProps.card)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await base.OnPlay(choiceContext, cardPlay);
+        await CreatureCmd.GainBlock(Owner.Creature, DynamicVars.Block, cardPlay);
+    }
+    public int MinimumCost => 1;
+    public int MaximumCost => IsUpgraded ? 3 : 6;
+    protected override void OnUpgrade()
+    {
+        base.OnUpgrade();
+        DynamicVars.Block.UpgradeValueBy(5);
+    }
+
+}
