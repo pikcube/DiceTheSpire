dd1e3817229ac56d737057dd59411d23f0c226cd
Cory Zink <cz109918@ohio.edu>
2026-07-22 23:51:04 -0400

    Added Starpick

---
+++ TheThief/TheThiefCode/Cards/Uncommon/Starpick.cs
@@ -0,0 +1,29 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Cards;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace TheThief.TheThiefCode.Cards.Uncommon;
+
+public class Starpick() : TheThiefCard(1, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(2)];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Cards.UpgradeValueBy(1);
+    }
+}
