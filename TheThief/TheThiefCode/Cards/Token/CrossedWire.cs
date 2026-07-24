acfa6b46f0c62afae6f16c027d52dac558f03087
Cory Zink <cz109918@ohio.edu>
2026-07-24 05:30:04 -0400

    Added Autotune, Roll the Bones, and Crossed Wire

---
+++ TheThief/TheThiefCode/Cards/Token/CrossedWire.cs
@@ -0,0 +1,35 @@
+﻿using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Orbs;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace TheThief.TheThiefCode.Cards.Token;
+
+[Pool(typeof(TokenCardPool))]
+public class CrossedWire() : TheThiefCard(2, CardType.Attack, CardRarity.Token, TargetType.AllEnemies)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5, ValueProp.Move), new RepeatVar(2)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.Channeling), HoverTipFactory.FromOrb<PlasmaOrb>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, cardPlay).TargetingAllOpponents(CombatState)
+            .WithHitCount(DynamicVars.Repeat.IntValue).Execute(choiceContext);
+        await OrbCmd.Channel<PlasmaOrb>(choiceContext, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(2);
+    }
+}
---
+++ TheThief/TheThiefCode/Cards/Token/RollTheBones.cs
@@ -0,0 +1,66 @@
+﻿using BaseLib.Utils;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.Models.Powers;
+
+namespace TheThief.TheThiefCode.Cards.Token;
+
+[Pool(typeof(TokenCardPool))]
+public class RollTheBones() : TheThiefCard(-1, CardType.Skill, CardRarity.Token, TargetType.AnyEnemy), ICountdown
+{
+    public int MaxCount
+    {
+        get;
+        set
+        {
+            int changeBy = value - field;
+            field = value;
+            CurrentCount += changeBy;
+        }
+    } = 3;
+
+    public int CurrentCount
+    {
+        get => DynamicVars[nameof(CurrentCount)].IntValue;
+        set
+        {
+            DynamicVars[nameof(CurrentCount)].BaseValue = value;
+            if (DynamicVars[nameof(CurrentCount)].BaseValue < 0)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = 0;
+            }
+
+            if (DynamicVars[nameof(CurrentCount)].BaseValue > MaxCount)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = MaxCount;
+            }
+        }
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars =>
+    [
+        new IntVar(nameof(CurrentCount), 3), new SummonVar(5), new PowerVar<DoomPower>(8)
+    ];
+
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(StaticHoverTip.SummonDynamic, DynamicVars.Summon), HoverTipFactory.FromPower<DoomPower>()];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await OstyCmd.Summon(choiceContext, Owner, DynamicVars.Summon.BaseValue, this);
+        await PowerCmd.Apply<DoomPower>(choiceContext, cardPlay.Target, DynamicVars.Doom.BaseValue, Owner.Creature,
+            this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Summon.UpgradeValueBy(2);
+        DynamicVars.Doom.UpgradeValueBy(4);
+    }
+}
