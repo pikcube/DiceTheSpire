ace61da3ac6e468edbc47d291d19d0998471fe2c
Pikcube <michaelds13@gmail.com>
2026-07-30 12:34:30 -0400

    Adjusted Fury

--- DiceTheSpireCore/DiceTheSpireCoreCode/Powers/FuryPower.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Powers/FuryPower.cs
@@ -1,6 +1,8 @@
 ﻿using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 
 namespace DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
@@ -8,17 +10,48 @@ public class FuryPower : DiceTheSpireCorePower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
+    private List<CardModel> CardsModifiedByFury { get; set; } = [];
+
     private bool _shouldIgnoreNextRemoval;
+
+    protected override void AfterCloned()
+    {
+        //Anytime you override AfterCloned, call the base method or bad things happen
+        base.AfterCloned();
+        //Since a List is a reference type, we need to insure that we get a brand
+        //new empty list instead of a reference to the canonical model's list
+        CardsModifiedByFury = [];
+    }
+
     public override int ModifyCardPlayCount(CardModel card, Creature? target, int playCount)
     {
         //return card.Owner.Creature == this.Owner ? playCount: playCount + 1;
         //|| CombatManager.Instance.History.CardPlaysStarted.Count<CardPlayStartedEntry>((Func<CardPlayStartedEntry, bool>)(e => e.Actor == this.Owner && e.HappenedThisTurn(this.CombatState))) >= this.Amount
-        return card.Owner.Creature != Owner  ? playCount : playCount + 1;
+        return card.Owner.Creature == Owner ? playCount + 1 : playCount;
     }
 
-    public override async Task AfterModifyingCardPlayCount(CardModel card)
+    public override Task AfterModifyingCardPlayCount(CardModel card)
     {
         Flash();
+        //This runs before the card is played, but we can't remove the power until after the card play
+        //resolves due to Flaming Sword, so we defer that call to AfterCardPlayed
+        CardsModifiedByFury.Add(card);
+        return Task.CompletedTask;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (!cardPlay.IsLastInSeries)
+        {
+            return;
+        }
+
+        //The remove function return false if the item wasn't in the list, and we don't want to decrement unless the card was modified by Fury
+        if (!CardsModifiedByFury.Remove(cardPlay.Card))
+        {
+            return;
+        }
+
         if (_shouldIgnoreNextRemoval)
         {
             _shouldIgnoreNextRemoval = false;
