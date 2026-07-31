8959bde90871ec0317ddf04e34127b38f82407b2
Corey <coreySegiethMail@gmail.com>
2026-07-30 22:16:59 -0400

    Added Knuckle Duster. Mover Tazer to the uncommon pool.

--- TheWarrior/TheWarriorCode/Cards/Uncommon/KnuckleDuster.cs
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/KnuckleDuster.cs
@@ -1,5 +1,4 @@
 ﻿using BaseLib.Extensions;
-using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
 using DiceTheSpireCore.DiceTheSpireCoreCode.Utilities;
 using MegaCrit.Sts2.Core.CardSelection;
 using MegaCrit.Sts2.Core.Commands;
@@ -9,16 +8,17 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace TheWarrior.TheWarriorCode.Cards.Common;
-public class Tazer() : TheWarriorCard(0, CardType.Skill, CardRarity.Common, TargetType.Self)
+
+public class KnuckleDuster() : TheWarriorCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(999), new PowerVar<ShockPower>(2)];
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Rummage), HoverTipFactory.FromPower<ShockPower>(DynamicVars.Power<ShockPower>().IntValue)];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(4), new PowerVar<VigorPower>(4M)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Rummage), HoverTipFactory.FromPower<VigorPower>(DynamicVars.Power<VigorPower>().IntValue)];
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<ShockPower>(choiceContext, Owner.Creature, DynamicVars.Power<ShockPower>().IntValue, Owner.Creature, this);
+        await PowerCmd.Apply<VigorPower>(choiceContext, Owner.Creature, DynamicVars.Power<VigorPower>().IntValue, Owner.Creature, this);
 
         CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_DISCARD"), 0, DynamicVars.Cards.IntValue);
         CardModel[] cards = [.. await CardSelectCmd.FromHand(choiceContext, Owner, cardSelectorPrefs, null, this)];
@@ -33,10 +33,9 @@ public class Tazer() : TheWarriorCard(0, CardType.Skill, CardRarity.Common, Targ
         }
 
         await CardPileCmd.Draw(choiceContext, cards.Length, Owner);
+
     }
 
-    protected override void OnUpgrade()
-    {
-        DynamicVars.Power<ShockPower>().UpgradeValueBy(-1);
-    }
+    protected override void OnUpgrade() => AddKeyword(CardKeyword.Retain);
+
 }
---
+++ TheWarrior/TheWarriorCode/Cards/Uncommon/Tazer.cs
@@ -0,0 +1,42 @@
+﻿using BaseLib.Extensions;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Utilities;
+using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+
+namespace TheWarrior.TheWarriorCode.Cards.Uncommon;
+public class Tazer() : TheWarriorCard(0, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(999), new PowerVar<ShockPower>(2)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Rummage), HoverTipFactory.FromPower<ShockPower>(DynamicVars.Power<ShockPower>().IntValue)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Retain];
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<ShockPower>(choiceContext, Owner.Creature, DynamicVars.Power<ShockPower>().IntValue, Owner.Creature, this);
+
+        CardSelectorPrefs cardSelectorPrefs = new(new LocString("card_selection", "TO_DISCARD"), 0, DynamicVars.Cards.IntValue);
+        CardModel[] cards = [.. await CardSelectCmd.FromHand(choiceContext, Owner, cardSelectorPrefs, null, this)];
+        foreach (CardModel card in cards)
+        {
+            await CardCmd.Discard(choiceContext, card);
+        }
+
+        if (cards.Length == 0)
+        {
+            return;
+        }
+
+        await CardPileCmd.Draw(choiceContext, cards.Length, Owner);
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Power<ShockPower>().UpgradeValueBy(-1);
+    }
+}
