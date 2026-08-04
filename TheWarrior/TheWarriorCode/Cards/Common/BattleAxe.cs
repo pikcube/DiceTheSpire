40aaf104ca74a5d3b72d7569b4ca6945c749cd6c
Corey <coreySegiethMail@gmail.com>
2026-08-03 20:53:32 -0400

    Added Nightstick. Adjusted some hover tips.

--- TheWarrior/TheWarriorCode/Cards/Common/BattleAxe.cs
+++ TheWarrior/TheWarriorCode/Cards/Common/BattleAxe.cs
@@ -1,9 +1,12 @@
 ﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
 using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using DiceTheSpireCore.DiceTheSpireCoreCode.Utilities;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace TheWarrior.TheWarriorCode.Cards.Common;
@@ -11,6 +14,7 @@ namespace TheWarrior.TheWarriorCode.Cards.Common;
 public class BattleAxe() : TheWarriorCard(2, CardType.Attack, CardRarity.Common, TargetType.AnyEnemy), IRangeCard
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5, DamageProps.card), new RepeatVar(2)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [BetterStaticHoverTips.RangeHoverTip(this)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
