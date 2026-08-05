ef47a2828ae9869f5134590a839480c6c7dd7c7e
Cory Zink <cz109918@ohio.edu>
2026-08-04 20:27:05 -0400

    Added Bell Peppers

---
+++ TheThief/TheThiefCode/Powers/BellPeppersStrengthPower.cs
@@ -0,0 +1,11 @@
+﻿using BaseLib.Abstracts;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Models.Powers;
+using TheThief.TheThiefCode.Cards.Common;
+
+namespace TheThief.TheThiefCode.Powers;
+
+public class BellPeppersStrengthPower : TemporaryStrengthPower, ICustomPower
+{
+    public override AbstractModel OriginModel => ModelDb.Card<BellPeppers>();
+}
