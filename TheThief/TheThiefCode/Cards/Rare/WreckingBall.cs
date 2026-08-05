0bb114d519fe3f8df35e3812aabda429f86bcf71
Cory Zink <cz109918@ohio.edu>
2026-08-05 17:24:51 -0400

    Changed Wrecking Ball to Rare. Now reduces 1 contdown to 0 instead of reducing all by 1.

--- TheThief/TheThiefCode/Cards/Rare/WreckingBall.cs
+++ TheThief/TheThiefCode/Cards/Rare/WreckingBall.cs
@@ -9,11 +9,11 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.ValueProps;
 
-namespace TheThief.TheThiefCode.Cards.Uncommon;
+namespace TheThief.TheThiefCode.Cards.Rare;
 
-public class WreckingBall() : TheThiefCard(1, CardType.Attack, CardRarity.Uncommon, TargetType.AnyEnemy)
+public class WreckingBall() : TheThiefCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(8, ValueProp.Move)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(9, ValueProp.Move)];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
@@ -25,12 +25,17 @@ public class WreckingBall() : TheThiefCard(1, CardType.Attack, CardRarity.Uncomm
 
         await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, cardPlay).Targeting(cardPlay.Target)
             .Execute(choiceContext);
-        foreach (CardModel card in Owner.PlayerCombatState.Hand.Cards)
+        CardModel? card = Owner.RunState.Rng.CombatCardSelection.NextItem(PileType.Hand.GetPile(Owner).Cards.Where(c =>
         {
-            if (card is ICountdown c)
+            if (c is ICountdown count)
             {
-                await c.DecrementCountAsync();
+                return count.CurrentCount > 0;
             }
+            return false;
+        }));
+        if (card is ICountdown countdown)
+        {
+            await countdown.DecrementCountAsync(countdown.CurrentCount);
         }
     }
 
