be02ac151d5ca407e9ec3babb4ebf9e6c4a30d57
Pikcube <michaelds13@gmail.com>
2026-08-07 11:21:21 -0400

    Rewrote Furious Form to use a hook because having a power reference hard coded into a power is bad practice

--- DiceTheSpireCore/DiceTheSpireCoreCode/Powers/FuriousFormPower.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Powers/FuriousFormPower.cs
@@ -1,9 +1,18 @@
-﻿using MegaCrit.Sts2.Core.Entities.Powers;
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Utilities;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.Models;
 
 namespace DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
-public class FuriousFormPower : DiceTheSpireCorePower
+public class FuriousFormPower : DiceTheSpireCorePower, IModifyFuryPlayCountListener
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
 
+    public void ModifyFuryPlayCount(FuryPower furyPower, CardModel card, ref int furyCount)
+    {
+        if (furyPower.Owner == Owner)
+        {
+            furyCount += Amount;
+        }
+    }
 }
