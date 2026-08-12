38a45e14154a70407e0256cc7028248c879affbd
Corey <coreySegiethMail@gmail.com>
2026-08-12 18:18:04 -0400

    Added Best Defense

---
+++ TheWarrior/TheWarriorCode/Cards/Rare/BestDefense.cs
@@ -0,0 +1,23 @@
+﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace TheWarrior.TheWarriorCode.Cards.Rare;
+public class BestDefense() : TheWarriorCard(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<IsAGoodOffensePower>(1M)];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Retain];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await PowerCmd.Apply<IsAGoodOffensePower>(choiceContext, Owner.Creature, 1M, Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        RemoveKeyword(CardKeyword.Exhaust);
+    }
+}
+
