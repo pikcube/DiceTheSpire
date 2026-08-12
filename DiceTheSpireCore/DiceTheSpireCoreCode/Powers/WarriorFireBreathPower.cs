733d67647045d0078b56b089494b8a84363eda9c
Pikcube <michaelds13@gmail.com>
2026-08-12 17:20:51 -0400

    Cleaned up Fire Breath to be multiplayer friendly

--- DiceTheSpireCore/DiceTheSpireCoreCode/Powers/WarriorFireBreathPower.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Powers/WarriorFireBreathPower.cs
@@ -1,10 +1,7 @@
-﻿using BaseLib.Extensions;
-using MegaCrit.Sts2.Core.Commands;
+﻿using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
-using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models;
 using MegaCrit.Sts2.Core.Models.Powers;
 
@@ -17,15 +14,10 @@ public class WarriorFireBreathPower : DiceTheSpireCorePower
     //protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<FuryPower>(DynamicVars.Power<FuryPower>().IntValue), HoverTipFactory.FromPower<VigorPower>(DynamicVars.Power<VigorPower>().IntValue)];
     public override async Task AfterPowerAmountChanged(PlayerChoiceContext choiceContext, PowerModel power, decimal amount, Creature? applier, CardModel? cardSource)
     {
-        if(power.Owner is null || power is null || Owner is null)
+        if (power.Owner != Owner || power is not FuryPower)
         {
             return;
         }
-        if (power is FuryPower) //and power.Owner == Owner
-        {
-            await PowerCmd.Apply<VigorPower>(new ThrowingPlayerChoiceContext(), Owner, this.Amount, Applier, null);
-        }
+        await PowerCmd.Apply<VigorPower>(choiceContext, Owner, Amount, Applier, null);
     }
-
-
 }
