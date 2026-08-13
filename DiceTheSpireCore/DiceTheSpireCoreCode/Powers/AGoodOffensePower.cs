fd5b0172902b50586753b12961120ec54fcc7de2
Pikcube <michaelds13@gmail.com>
2026-08-13 10:53:10 -0400

    Renamed IsAGoodOffensePower to AGoodOffensePower since it is not a boolean

--- DiceTheSpireCore/DiceTheSpireCoreCode/Powers/AGoodOffensePower.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Powers/AGoodOffensePower.cs
@@ -1,25 +1,21 @@
-﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Interfaces;
-using DiceTheSpireCore.DiceTheSpireCoreCode.Utilities;
-using MegaCrit.Sts2.Core.Combat;
+﻿using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
-using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
 
 
 namespace DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
-public class IsAGoodOffensePower : DiceTheSpireCorePower
+public class AGoodOffensePower : DiceTheSpireCorePower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
 
     public override async Task AfterDamageGiven(PlayerChoiceContext choiceContext, Creature? dealer, DamageResult result, ValueProp props, Creature target, CardModel? cardSource)
     {
-        if(result is null || dealer != Owner)
+        if(dealer != Owner)
         {  
             return; 
         }
--- TheWarrior/TheWarriorCode/Cards/Rare/BestDefense.cs
+++ TheWarrior/TheWarriorCode/Cards/Rare/BestDefense.cs
@@ -7,12 +7,12 @@ using MegaCrit.Sts2.Core.Localization.DynamicVars;
 namespace TheWarrior.TheWarriorCode.Cards.Rare;
 public class BestDefense() : TheWarriorCard(1, CardType.Skill, CardRarity.Rare, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<IsAGoodOffensePower>(1M)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<AGoodOffensePower>(1M)];
     public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust, CardKeyword.Retain];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<IsAGoodOffensePower>(choiceContext, Owner.Creature, 1M, Owner.Creature, this);
+        await PowerCmd.Apply<AGoodOffensePower>(choiceContext, Owner.Creature, 1M, Owner.Creature, this);
     }
 
     protected override void OnUpgrade()
