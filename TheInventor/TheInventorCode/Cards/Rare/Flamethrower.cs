8a58af19cbed86f55684dc249cef10d76e80c48c
Pikcube <michaelds13@gmail.com>
2026-08-21 16:13:56 -0400

    Added fire to the flamethrower

--- TheInventor/TheInventorCode/Cards/Rare/Flamethrower.cs
+++ TheInventor/TheInventorCode/Cards/Rare/Flamethrower.cs
@@ -1,8 +1,12 @@
 ﻿using BaseLib.Extensions;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Nodes.Rooms;
+using MegaCrit.Sts2.Core.Nodes.Vfx;
 using MegaCrit.Sts2.Core.ValueProps;
 using TheInventor.TheInventorCode.Gadgets;
 
@@ -28,13 +32,21 @@ public class Flamethrower() : TheInventorCard(2, CardType.Attack, CardRarity.Rar
             .FromCard(this, cardPlay)
             .TargetingAllOpponents(CombatState)
             .WithValueProp(DynamicVars.Damage.Props)
-            .WithHitFx(VfxCmd.slashPath)
+            .BeforeDamage(() =>
+            {
+                foreach (Creature c in CombatState.Enemies.ToArray())
+                {
+                    NCombatRoom.Instance?.CombatVfxContainer.AddChildSafely(NGroundFireVfx.Create(c));
+                }
+
+                return Task.CompletedTask;
+            })
             .Execute(choiceContext);
     }
 
     protected override void OnUpgrade()
     {
         EnergyCost.UpgradeBy(2);
-        DynamicVars.Damage.UpgradeValueBy(18);
+        DynamicVars.Damage.UpgradeValueBy(30);
     }
 }
