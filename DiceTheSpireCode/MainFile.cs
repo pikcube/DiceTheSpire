00f3f3a2a2b09e9f726d2a2ec13449c17cd833de
Pikcube <michaelds13@gmail.com>
2026-09-02 17:12:27 -0400

    Prepared LocAliasManager for extraction

--- DiceTheSpireCode/MainFile.cs
+++ DiceTheSpireCode/MainFile.cs
@@ -3,10 +3,8 @@ using DiceTheSpire.DiceTheSpireCode.Inventor;
 using Godot;
 using HarmonyLib;
 using MegaCrit.Sts2.Core.Helpers;
-using MegaCrit.Sts2.Core.Localization;
 using MegaCrit.Sts2.Core.Logging;
 using MegaCrit.Sts2.Core.Modding;
-using System.Text.Json;
 using Logger = MegaCrit.Sts2.Core.Logging.Logger;
 
 namespace DiceTheSpire.DiceTheSpireCode;
@@ -15,8 +13,6 @@ namespace DiceTheSpire.DiceTheSpireCode;
 [ModInitializer(nameof(Initialize))]
 public partial class MainFile : Node
 {
-    public static List<LocAliasInfo> LocAliases { get; } = [];
-
     public const string ModId = "DiceTheSpire"; //At the moment, this is used only for the Logger and harmony names.
 
     public static Logger Logger { get; } = new(ModId, LogType.Generic);
@@ -31,11 +27,11 @@ public partial class MainFile : Node
         
         CustomLocTableManager.Register("gadgets.json");
 
-        
-        RegisterMerge("cards.json", "cards.inventor.json");
-        RegisterMerge("relics.json", "relics.inventor.json");
-        RegisterMerge("potions.json", "potions.inventor.json");
-        RegisterMerge("powers.json", "powers.inventor.json");
+
+        LocAliasManager.Register(ModId, "cards", "cards.inventor");
+        LocAliasManager.Register(ModId, "relics.json", "relics.inventor");
+        LocAliasManager.Register(ModId, "potions", "potions.inventor.json");
+        LocAliasManager.Register(ModId, "powers.json", "powers.inventor.json");
         
         CustomCharacterUtils.TryOrderCustomCharacters<
             TheWarrior.TheWarriorCode.Character.TheWarrior,
@@ -43,10 +39,21 @@ public partial class MainFile : Node
             TheInventor
         >();
     }
+}
 
-    private static void RegisterMerge(string path, params IEnumerable<string> aliases)
+[HarmonyPatch(typeof(ModManager), nameof(ModManager.GetModdedLocTables))]
+public static class LocAliasManager
+{
+    private static List<LocAliasInfo> LocAliases { get; } = [];
+
+    public static void Register(string modId, string path, params IEnumerable<string> aliases)
     {
-        DirAccess directory = DirAccess.Open(Path.Join(ResPath, "localization"));
+        if (!path.EndsWith(".json"))
+        {
+            path = $"{path}.json";
+        }
+
+        DirAccess directory = DirAccess.Open(Path.Join($"res://{modId}", "localization"));
 
         string[] languages = directory.GetDirectories();
 
@@ -54,8 +61,18 @@ public partial class MainFile : Node
 
         foreach (string language in languages)
         {
-            string basePath = string.Join('/', ResPath, "localization", language, path);
-            IEnumerable<string> aliasPaths = aliasArray.Select(s => string.Join('/', ResPath, "localization", language, s));
+            string basePath = string.Join('/', "res://localization", language, path);
+            IEnumerable<string> aliasPaths = aliasArray
+                .Select(s =>
+                {
+                    if (!s.EndsWith(".json"))
+                    {
+                        s = $"{s}.json";
+                    }
+
+                    return string.Join('/', $"res://{modId}", "localization", language, s);
+                })
+                .Where(s => ResourceLoader.Exists(s));
 
             LocAliasInfo? existing = LocAliases.SingleOrDefault(lai => lai.BasePath == basePath);
 
@@ -69,38 +86,23 @@ public partial class MainFile : Node
             }
         }
     }
-}
 
-[HarmonyPatch(typeof(LocManager), "LoadTable")]
-public static class MergeTablePatch
-{
     [HarmonyPostfix]
-    public static void MergeAliasesIntoTable(ref Dictionary<string, string> __result, string path)
+    internal static IEnumerable<string> MergeAliasesIntoTable(IEnumerable<string> __result, string language, string file)
     {
-        foreach (LocAliasInfo info in MainFile.LocAliases.Where(lai => lai.BasePath == path))
+        string path = string.Join('/', "res://localization", language, file);
+        foreach (string original in __result)
+        {
+            yield return original;
+        }
+        foreach (LocAliasInfo info in LocAliases.Where(lai => lai.BasePath == path))
         {
             foreach (string alias in info.AliasPaths)
             {
-                using Godot.FileAccess? fileAccess = Godot.FileAccess.Open(alias, Godot.FileAccess.ModeFlags.Read);
-                if (fileAccess is null)
-                {
-                    continue;
-                }
-
-                string json = fileAccess.GetAsText();
-                Dictionary<string, string>? newDict = JsonSerializer.Deserialize<Dictionary<string, string>>(json);
-                if (newDict is null)
-                {
-                    continue;
-                }
-
-                foreach ((string key, string value) in newDict)
-                {
-                    __result[key] = value;
-                }
+                yield return alias;
             }
         }
     }
 }
 
-public record LocAliasInfo(string BasePath, List<string> AliasPaths);
+internal record LocAliasInfo(string BasePath, List<string> AliasPaths);
