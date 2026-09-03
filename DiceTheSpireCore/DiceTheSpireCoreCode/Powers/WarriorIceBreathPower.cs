17deb4e4626e5da9db93bd3d5667df44fc43d85a
Corey <coreySegiethMail@gmail.com>
2026-09-03 00:10:49 -0400

    Fixed Ice Breath

--- DiceTheSpireCore/DiceTheSpireCoreCode/Powers/WarriorIceBreathPower.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/Powers/WarriorIceBreathPower.cs
@@ -1,7 +1,9 @@
 ﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.ValueProps;
 
 namespace DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
 public class WarriorIceBreathPower : DiceTheSpireCorePower
@@ -10,10 +12,13 @@ public class WarriorIceBreathPower : DiceTheSpireCorePower
     public override PowerStackType StackType => PowerStackType.Counter;
     public override async Task BeforeSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
     {
-        //if (Owner != Owner.Powers.Contains(Powers.FuryPower))
-        //{
-        //    return;
-        //}
-        //await CreatureCmd.GainBlock(Owner, Amount, BlockProps.nonCardUnpowered, null);
+        if (side != Owner.Side)
+        {
+            return;
+        }
+        if (Owner.HasPower<FuryPower>())
+        {
+            await CreatureCmd.GainBlock(Owner, Amount, BlockProps.nonCardUnpowered, null);
+        }
     }
 }
