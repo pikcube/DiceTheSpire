cc33a4a24fea08248c8ac6a60004408af3f9c56e
Pikcube <michaelds13@gmail.com>
2026-09-02 23:05:55 -0400

    Fixed bug in keyword text

--- DiceTheSpireCore/DiceTheSpireCoreCode/MainFile.cs
+++ DiceTheSpireCore/DiceTheSpireCoreCode/MainFile.cs
@@ -15,7 +15,7 @@ namespace DiceTheSpireCore.DiceTheSpireCoreCode;
 public partial class MainFile : Node
 {
     public const string ModId = "DiceTheSpireCore"; //Used for resource filepath
-    public static string ModPrefix { get; } = StringHelper.Slugify(ModId);
+    public static string ModPrefix { get; } = ModId.ToUpperInvariant();
     public const string ResPath = $"res://{ModId}";
 
     public static Logger Logger { get; } = new(ModId, LogType.Generic);
