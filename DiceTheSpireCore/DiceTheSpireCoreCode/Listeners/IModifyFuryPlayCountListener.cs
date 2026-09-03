7711f2630e4e9176a3db5b91d8b324ab534d1535
Pikcube <michaelds13@gmail.com>
2026-09-03 02:16:56 -0400

    Cleared out redundant files, reset uids

--- DiceTheSpireCore/DiceTheSpireCoreCode/Listeners/IModifyFuryPlayCountListener.cs
+++
@@ -1,9 +0,0 @@
-﻿using DiceTheSpireCore.DiceTheSpireCoreCode.Powers;
-using MegaCrit.Sts2.Core.Models;
-
-namespace DiceTheSpireCore.DiceTheSpireCoreCode.Listeners;
-
-public interface IModifyFuryPlayCountListener
-{
-    public void ModifyFuryPlayCount(FuryPower furyPower, CardModel card, ref int furyCount);
-}
--- DiceTheSpireCore/DiceTheSpireCoreCode/Listeners/IModifyScrapPriorityListener.cs
+++
@@ -1,9 +0,0 @@
-﻿using MegaCrit.Sts2.Core.Entities.Players;
-using MegaCrit.Sts2.Core.Models;
-
-namespace DiceTheSpireCore.DiceTheSpireCoreCode.Listeners;
-
-public interface IModifyScrapPriorityListener
-{
-    public void ModifyPriority(Player player, ref List<CardModel> scrapCards, ref List<CardModel> otherCards);
-}
