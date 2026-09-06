da77ed44b98ea4823a0714a3fe977059ebadda9a
Cory Zink <cz109918@ohio.edu>
2026-09-06 16:35:26 -0400

    Added Tiny Trebuchet

---
+++ Shared/Relics/TinyTrebuchet.cs
@@ -0,0 +1,33 @@
+﻿using DiceTheSpire.Shared.Listeners;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.ValueProps;
+using Pikcube.Common.Extensions;
+
+namespace DiceTheSpire.Shared.Relics;
+
+public class TinyTrebuchet : TheThiefRelic, IModifyPipOnPlayListener
+{
+    public override RelicRarity Rarity => RelicRarity.Uncommon;
+
+    public LocString PipDescription => new LocString("relics", Id.Entry + ".pipDescription").WithDynamicVars(DynamicVars);
+
+    public IEnumerable<IHoverTip> PipHoverTips => [];
+
+    public async Task ModifyPipOnPlayAsync(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (Owner.Creature.CombatState is null || cardPlay.Card.Owner != Owner)
+        {
+            return;
+        }
+
+        await DamageCmd.Attack(4).FromCard(cardPlay.Card, cardPlay).TargetingRandomOpponents(Owner.Creature.CombatState)
+            .Execute(choiceContext);
+    }
+}
