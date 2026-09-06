b280f07ce735c6cc1542ae2b48fadc4bd60ab2bb
Cory Zink <cz109918@ohio.edu>
2026-09-06 17:12:07 -0400

    Tiny Trebuchet now implements ShouldModify

--- Shared/Relics/TinyTrebuchet.cs
+++ Shared/Relics/TinyTrebuchet.cs
@@ -1,12 +1,12 @@
-﻿using DiceTheSpire.Shared.Listeners;
-using MegaCrit.Sts2.Core.Combat;
+﻿using DiceTheSpire.Shared.Cards;
+using DiceTheSpire.Shared.Listeners;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.Entities.Relics;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.ValueProps;
 using Pikcube.Common.Extensions;
 
@@ -16,18 +16,30 @@ public class TinyTrebuchet : TheThiefRelic, IModifyPipOnPlayListener
 {
     public override RelicRarity Rarity => RelicRarity.Uncommon;
 
-    public LocString PipDescription => new LocString("relics", Id.Entry + ".pipDescription").WithDynamicVars(DynamicVars);
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(4, ValueProp.Move)];
+
+    public LocString PipDescription
+    {
+        get
+        {
+            LocString l = new LocString("relics", Id.Entry + ".pipDescription").WithDynamicVars(DynamicVars);
+            l.Add("Damage", DynamicVars.Damage.BaseValue);
+            return l;
+        }
+    }
 
     public IEnumerable<IHoverTip> PipHoverTips => [];
 
-    public async Task ModifyPipOnPlayAsync(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    public async Task ModifyPipOnPlayAsync(PlayerChoiceContext choiceContext, CardPlay cardPlay, Pip pip)
     {
-        if (Owner.Creature.CombatState is null || cardPlay.Card.Owner != Owner)
+        if (Owner.Creature.CombatState is null || !ShouldModify(pip))
         {
             return;
         }
 
-        await DamageCmd.Attack(4).FromCard(cardPlay.Card, cardPlay).TargetingRandomOpponents(Owner.Creature.CombatState)
-            .Execute(choiceContext);
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(pip, cardPlay).TargetingRandomOpponents(Owner.Creature.CombatState)
+            .Unpowered().Execute(choiceContext);
     }
+
+    public bool ShouldModify(Pip pip) => pip.Owner == Owner;
 }
