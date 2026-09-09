9b60267fac917094ab03e27e51f3f7cb90e59574
Corey <coreySegiethMail@gmail.com>
2026-09-09 00:11:05 -0400

    Added Cartwheel

---
+++ Warrior/WorkoutRewards/Cartwheel.cs
@@ -0,0 +1,41 @@
+﻿using DiceTheSpire.Shared.Commands;
+using DiceTheSpire.Shared.DynamicVars;
+using DiceTheSpire.Shared.Listeners;
+using DiceTheSpire.Shared.Utility;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+
+
+namespace DiceTheSpire.Warrior.WorkoutRewards;
+public class Cartwheel() : TheWarriorCard(3, CardType.Skill, CardRarity.Token, TargetType.Self), IAfterRerollListener
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Reroll)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+    }
+    public async Task AfterRerollAsync(CardModel card, bool isFixed, int originalCost, int getAmountToSpend, RerollDuration duration, PlayerChoiceContext choiceContext)
+    {
+        if (card == this)
+        {
+            await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
+        }
+    }
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Cards.UpgradeValueBy(1);
+    }
+
+    public Task AfterRerollAsync(CardModel card, bool isFixed, int originalCost, int getAmountToSpend, RerollDuration duration)
+    {
+        throw new NotImplementedException();
+    }
+}
