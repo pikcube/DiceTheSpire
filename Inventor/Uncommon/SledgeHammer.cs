556fb42e5e2d29cd27a68c86eda764da2cd9ed2e
Pikcube <michaelds13@gmail.com>
2026-09-09 17:51:40 -0400

    Sledge Hammer rework again

--- Inventor/Uncommon/SledgeHammer.cs
+++ Inventor/Uncommon/SledgeHammer.cs
@@ -1,23 +1,17 @@
 ﻿using DiceTheSpire.Inventor.Gadgets;
-using DiceTheSpire.Shared.Powers;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
-using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
-using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
-using Pikcube.Common.Extensions;
 
 namespace DiceTheSpire.Inventor.Uncommon;
 
 
 public class SledgeHammer() : TheInventorCard(-1, CardType.Attack, CardRarity.Uncommon, TargetType.AllEnemies)
 {
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Unplayable, CardKeyword.Innate, CardKeyword.Ethereal];
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(5, DamageProps.cardUnpowered), new PowerVar<VulnerablePower>(2)];
-
-    protected override IEnumerable<IHoverTip> ExtraInventorHoverTips => [HoverTipFactory.FromPower<VulnerablePower>()];
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Unplayable];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(10, DamageProps.cardUnpowered)];
 
     public override bool HasTurnEndInHandEffect => true;
 
@@ -25,18 +19,17 @@ public class SledgeHammer() : TheInventorCard(-1, CardType.Attack, CardRarity.Un
 
     protected override async Task OnTurnEndInHand(PlayerChoiceContext choiceContext)
     {
-        if (CombatState is null)
+        if (CombatState is null || CombatState.Enemies.Count == 0)
         {
             return;
         }
 
         await CreatureCmd.Damage(choiceContext, CombatState.Enemies, DynamicVars.Damage, Owner.Creature, this, null);
-        await NextTurnVulnerablePower.ApplyAsync(choiceContext, CombatState.Enemies, DynamicVars.Vulnerable.IntValue, Owner.Creature, this);
+        DynamicVars.Damage.UpgradeValueBy(-1);
     }
 
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(2);
-        DynamicVars.Vulnerable.UpgradeValueBy(2);
+        DynamicVars.Damage.UpgradeValueBy(4);
     }
 }
