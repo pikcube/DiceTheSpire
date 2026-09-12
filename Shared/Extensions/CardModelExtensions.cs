42ce93d93f2b373f486b80fb72e47abf32953b12
Pikcube <michaelds13@gmail.com>
2026-09-12 03:04:16 -0400

    Removed unused usings

--- Shared/Extensions/CardModelExtensions.cs
+++ Shared/Extensions/CardModelExtensions.cs
@@ -1,9 +1,7 @@
 ﻿using DiceTheSpire.Shared.Keywords;
 using DiceTheSpire.Shared.Utility;
-using MegaCrit.Sts2.Core.Combat;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
-using MegaCrit.Sts2.Core.Entities.Players;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.Models;
 using Pikcube.Common.Extensions;
