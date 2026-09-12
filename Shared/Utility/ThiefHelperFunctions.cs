f079da7edf8f155950e34b28db3e4808b1a58f72
Cory Zink <cz109918@ohio.edu>
2026-09-12 19:46:17 -0400

    Created helper function for creating off-class cards for thief.

---
+++ Shared/Utility/ThiefHelperFunctions.cs
@@ -0,0 +1,17 @@
+﻿using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Factories;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.Random;
+
+namespace DiceTheSpire.Shared.Utility;
+
+public static class ThiefHelperFunctions
+{
+    public static IEnumerable<CardModel> GetDistinctForCombatOfClass(Player player, IEnumerable<CharacterModel> characters, int count, Rng rng)
+    {
+        return characters.SelectMany(character => CardFactory.GetDistinctForCombat(player,
+            character.CardPool.GetUnlockedCards(player.UnlockState, player.RunState.CardMultiplayerConstraint), 
+            count, rng));
+    }
+}
