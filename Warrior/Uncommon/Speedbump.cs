329a612bfc9aa92bcebe6849c3eb1d0f6b8ce21b
Corey <coreySegiethMail@gmail.com>
2026-09-14 18:19:09 -0400

    Reworked Speedbump. It is now a power that bumps the first 3 discarded cards each turn. Why discarded? Because I tried every other varient and they either let you go infinite or felt clunky.

--- Warrior/Uncommon/Speedbump.cs
+++ Warrior/Uncommon/Speedbump.cs
@@ -8,35 +8,17 @@ using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
 namespace DiceTheSpire.Warrior.Uncommon;
-public class Speedbump() : TheWarriorCard(0, CardType.Skill, CardRarity.Uncommon, TargetType.Self)//, IRangeCard
+public class Speedbump() : TheWarriorCard(2, CardType.Power, CardRarity.Uncommon, TargetType.Self)
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(1), new PowerVar<SpeedbumpPower>(1M)];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [CardKeyword.Exhaust];
-    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Bump)]; //BetterStaticHoverTips.RangeHoverTip(this),
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<SpeedbumpPower>(3M)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Bump)]; 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         await PowerCmd.Apply<SpeedbumpPower>(choiceContext, Owner.Creature, DynamicVars.Power<SpeedbumpPower>().IntValue, Owner.Creature, this);
-
-        //var cards = 
-        await CardPileCmd.Draw(choiceContext, DynamicVars.Cards.BaseValue, Owner);
-        //foreach (CardModel card in cards)
-        //{
-            //await card.BumpAsync(choiceContext);
-            //if(IsUpgraded)
-            //{
-            //    if(card.EnergyCost.CostsX==false)
-            //    {
-            //        card.EnergyCost.SetThisTurnOrUntilPlayed(card.EnergyCost.GetAmountToSpend() - 1);
-            //    }
-            //}
-        //}
     }
-
-    //public int MinimumCost => 0;
-    //public int MaximumCost => 0;
     protected override void OnUpgrade()
     {
-        RemoveKeyword(CardKeyword.Exhaust);
+        EnergyCost.UpgradeBy(-1);
     }
 }
 
