4eeb1ab0580035d10a3f632fbe4c8daf0eff5364
Cory Zink <cz109918@ohio.edu>
2026-09-16 21:42:08 -0400

    Added Pocket Dice

---
+++ Shared/Relics/PocketDice.cs
@@ -0,0 +1,27 @@
+﻿using DiceTheSpire.Shared.Cards;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Entities.Players;
+using MegaCrit.Sts2.Core.Entities.Relics;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+
+namespace DiceTheSpire.Shared.Relics;
+
+public class PocketDice : TheThiefRelic
+{
+    public override RelicRarity Rarity => RelicRarity.Rare;
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+    protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<Pip>()];
+
+    public override async Task BeforeHandDraw(Player player, PlayerChoiceContext choiceContext, ICombatState combatState)
+    {
+        if (player != Owner || Owner.PlayerCombatState?.TurnNumber > 1)
+        {
+            return;
+        }
+
+        Flash();
+        await Pip.CreateInHandAsync(player, DynamicVars.Cards.IntValue, combatState);
+    }
+}
