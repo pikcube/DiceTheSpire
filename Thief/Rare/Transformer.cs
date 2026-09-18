3660eb1ee5e180efbee270c33d25eb90f82c3f7c
Cory Zink <cz109918@ohio.edu>
2026-09-18 04:24:39 -0400

    Created Transformer and moved its localization from cards.inventor to cards.thief

---
+++ Thief/Rare/Transformer.cs
@@ -0,0 +1,33 @@
+﻿using MegaCrit.Sts2.Core.CardSelection;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace DiceTheSpire.Thief.Rare;
+
+public class Transformer() : TheThiefCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+{
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(16, ValueProp.Move)];
+
+    protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        ArgumentNullException.ThrowIfNull(cardPlay.Target);
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue).FromCard(this, cardPlay).Targeting(cardPlay.Target)
+            .Execute(choiceContext);
+        foreach (CardModel card in await CardSelectCmd.FromHand(choiceContext, Owner,
+            new CardSelectorPrefs(CardSelectorPrefs.TransformSelectionPrompt, 1), null, this))
+        {
+            await CardCmd.Transform(card, CreateClone());
+        }
+        
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(4);
+    }
+}
