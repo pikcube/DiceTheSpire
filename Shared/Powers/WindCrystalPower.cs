71c99b3d891a4650c75f4a3420b36d913187a42c
Corey <coreySegiethMail@gmail.com>
2026-09-20 20:39:41 -0400

    Reworked Super Set to be Wind Crystal. Changed its rarity.

--- Shared/Powers/WindCrystalPower.cs
+++ Shared/Powers/WindCrystalPower.cs
@@ -4,7 +4,7 @@ using MegaCrit.Sts2.Core.Entities.Powers;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 
 namespace DiceTheSpire.Shared.Powers;
-public class SuperSetRummagePower : DiceTheSpirePower
+public class WindCrystalPower : DiceTheSpirePower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
--- Warrior/Uncommon/WindCrystal.cs
+++ Warrior/Uncommon/WindCrystal.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLib.Extensions;
+using DiceTheSpire.Shared.Interfaces;
 using DiceTheSpire.Shared.Powers;
 using DiceTheSpire.Shared.Utility;
 using MegaCrit.Sts2.Core.Commands;
@@ -7,20 +8,20 @@ using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
-namespace DiceTheSpire.Warrior.Rare;
+namespace DiceTheSpire.Warrior.Uncommon;
 
-public class SuperSet() : TheWarriorCard(1, CardType.Power, CardRarity.Rare, TargetType.Self)
+public class WindCrystal() : TheWarriorCard(1, CardType.Power, CardRarity.Uncommon, TargetType.Self), ICrystalCard
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<SuperSetRummagePower>(2M)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<WindCrystalPower>(1M)];
     //PowerVar<SuperSetDrawPower>(1M);
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Rummage)];
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        await PowerCmd.Apply<SuperSetRummagePower>(choiceContext, Owner.Creature, DynamicVars.Power<SuperSetRummagePower>().IntValue, Owner.Creature, this);
+        await PowerCmd.Apply<WindCrystalPower>(choiceContext, Owner.Creature, DynamicVars.Power<WindCrystalPower>().IntValue, Owner.Creature, this);
         //await PowerCmd.Apply<SuperSetDrawPower>(choiceContext, Owner.Creature, DynamicVars.Power<SuperSetDrawPower>().IntValue, Owner.Creature, this);
     }
     protected override void OnUpgrade()
     {
-        DynamicVars.Power<SuperSetRummagePower>().UpgradeValueBy(1);
+        DynamicVars.Power<WindCrystalPower>().UpgradeValueBy(1);
     }
 }
