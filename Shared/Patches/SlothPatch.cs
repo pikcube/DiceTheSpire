5d05acef08def78a86afccb6264ac75cbb076797
Pikcube <michaelds13@gmail.com>
2026-09-23 13:05:56 -0400

    Artifact is bull shit

---
+++ Shared/Patches/SlothPatch.cs
@@ -0,0 +1,30 @@
+﻿using BaseLib.Abstracts;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace DiceTheSpire.Shared.Patches;
+
+[HarmonyPatch(typeof(Sloth), nameof(Sloth.OnChosen))]
+public class SlothPatch() : CustomSingletonModel(HookType.Combat)
+{
+    public static bool IsKnowledgeDemonDoingStuff => SlothsInProgress.Count > 0;
+
+    private static List<Sloth> SlothsInProgress { get; } = [];
+
+    public override Task BeforeCombatStart()
+    {
+        SlothsInProgress.Clear();
+        return Task.CompletedTask;
+    }
+
+    public static void Prefix(Sloth __instance)
+    {
+        SlothsInProgress.Add(__instance);
+    }
+
+    public static async Task Postfix(Task __result, Sloth __instance)
+    {
+        await __result;
+        SlothsInProgress.Remove(__instance);
+    }
+}
---
+++ Shared/Patches/WasteAwayPatch.cs
@@ -0,0 +1,30 @@
+﻿using BaseLib.Abstracts;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Models.Cards;
+
+namespace DiceTheSpire.Shared.Patches;
+
+[HarmonyPatch(typeof(WasteAway), nameof(WasteAway.OnChosen))]
+public class WasteAwayPatch() : CustomSingletonModel(HookType.Combat)
+{
+    public static bool IsKnowledgeDemonDoingStuff => WasteAwaysInProgress.Count > 0;
+
+    private static List<WasteAway> WasteAwaysInProgress { get; } = [];
+
+    public override Task BeforeCombatStart()
+    {
+        WasteAwaysInProgress.Clear();
+        return Task.CompletedTask;
+    }
+
+    public static void Prefix(WasteAway __instance)
+    {
+        WasteAwaysInProgress.Add(__instance);
+    }
+
+    public static async Task Postfix(Task __result, WasteAway __instance)
+    {
+        await __result;
+        WasteAwaysInProgress.Remove(__instance);
+    }
+}
