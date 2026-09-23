3a673d33a8926bab67768e2fc1344c93b266144e
Pikcube <michaelds13@gmail.com>
2026-09-23 01:20:25 -0400

    Added PowerModel.CanonicalInstance

---
+++ Shared/Extensions/PowerModelExtensions.cs
@@ -0,0 +1,12 @@
+﻿using MegaCrit.Sts2.Core.Models;
+using Pikcube.Common.Extensions;
+
+namespace DiceTheSpire.Shared.Extensions;
+
+public static class PowerModelExtensions
+{
+    extension<T>(T instance) where T : PowerModel
+    {
+        public PowerModel CanonicalInstance => ModelDb.GetModel<T>(instance.GetType());
+    }
+}
