331fc7e76dde94355be5b2f8eafb3d49b6cdaa67
Pikcube <michaelds13@gmail.com>
2026-09-23 11:13:08 -0400

    Fixed Rubber Mallet to use a var

--- Warrior/Rare/RubberMallet.cs
+++ Warrior/Rare/RubberMallet.cs
@@ -12,7 +12,7 @@ using MegaCrit.Sts2.Core.ValueProps;
 namespace DiceTheSpire.Warrior.Rare;
 public class RubberMallet() : TheWarriorCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy), IAfterRerollListener
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6, DamageProps.card)];
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6, DamageProps.card), new ExtraDamageVar(2)];
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Reroll)];
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
@@ -26,14 +26,14 @@ public class RubberMallet() : TheWarriorCard(1, CardType.Attack, CardRarity.Rare
         
     }
 
-    public async Task AfterRerollAsync(PlayerChoiceContext choiceContext, CardModel card, bool isFixed, int originalCost, int getAmountToSpend, RerollDuration duration)
+    public Task AfterRerollAsync(PlayerChoiceContext choiceContext, CardModel card, bool isFixed, int originalCost, int getAmountToSpend, RerollDuration duration)
     {
-        DynamicVars.Damage.UpgradeValueBy(2);
-        if(IsUpgraded)
-        DynamicVars.Damage.UpgradeValueBy(1);
+        DynamicVars.Damage.UpgradeValueBy(DynamicVars.ExtraDamage.BaseValue);
+        return Task.CompletedTask;
     }
     protected override void OnUpgrade()
     {
         DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars.ExtraDamage.UpgradeValueBy(1);
     }
 }
