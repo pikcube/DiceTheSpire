c97d972dda72c230b2d66e215b5e10f99f181075
Pikcube <michaelds13@gmail.com>
2026-09-22 20:57:36 -0400

    Fixed target type on spear

--- Inventor/Rare/RosewoodSpear.cs
+++ Inventor/Rare/RosewoodSpear.cs
@@ -1,39 +1,53 @@
-﻿using DiceTheSpire.Inventor.Gadgets;
+﻿using BaseLib.Extensions;
+using DiceTheSpire.Inventor.Gadgets;
 using DiceTheSpire.Shared.Keywords;
 using MegaCrit.Sts2.Core.Commands;
-using MegaCrit.Sts2.Core.Commands.Builders;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
 using MegaCrit.Sts2.Core.Models.Powers;
 using MegaCrit.Sts2.Core.ValueProps;
-using Pikcube.Common.Extensions;
 
 namespace DiceTheSpire.Inventor.Rare;
 
 
-public class RosewoodSpear() : TheInventorCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy)
+public class RosewoodSpear() : TheInventorCard(1, CardType.Attack, CardRarity.Rare, TargetType.AllEnemies)
 {
     protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(4, DamageProps.card)];
-    public override IEnumerable<CardKeyword> CanonicalKeywords => [ShockModel.Shock];
 
     protected override IEnumerable<IHoverTip> ExtraInventorHoverTips => [HoverTipFactory.FromPower<ThornsPower>()];
     public override string GetScrapId => nameof(Needle);
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
-        ArgumentNullException.ThrowIfNull(cardPlay.Target);
 
-        AttackCommand cmd = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+        if (CombatState is null)
+        {
+            return;
+        }
+
+        ThornsPower? thorns = Owner.Creature.GetPower<ThornsPower>();
+        if (thorns is null)
+        {
+            return;
+        }
+
+        await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
             .FromCard(this, cardPlay)
-            .Targeting(cardPlay.Target)
+            .TargetingAllOpponents(CombatState)
+            .WithValueProp(DynamicVars.Damage.Props)
             .WithHitFx(VfxCmd.slashPath)
+            .WithHitCount(thorns.Amount)
             .Execute(choiceContext);
+    }
 
-        int total = cmd.Results.SelectMany(resultList => resultList).Select(result => result.UnblockedDamage).Sum();
-
-        await ThornsPower.ApplyAsync(choiceContext, Owner.Creature, total, Owner.Creature, this);
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card == this && cardPlay.IsLastInSeries)
+        {
+            await PowerCmd.Remove<ThornsPower>(Owner.Creature);
+        }
     }
 
     protected override void OnUpgrade()
