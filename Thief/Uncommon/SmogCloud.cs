2fd672c82077dce24e9bd6fd00f3fb18e7cefd35
Cory Zink <cz109918@ohio.edu>
2026-09-23 23:47:04 -0400

    Smog Cloud is now a countdown card.

--- Thief/Uncommon/SmogCloud.cs
+++ Thief/Uncommon/SmogCloud.cs
@@ -1,4 +1,5 @@
-﻿using MegaCrit.Sts2.Core.Commands;
+﻿using DiceTheSpire.Shared.Interfaces;
+using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
@@ -7,9 +8,38 @@ using MegaCrit.Sts2.Core.Models.Powers;
 
 namespace DiceTheSpire.Thief.Uncommon;
 
-public class SmogCloud() : TheThiefCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies)
+public class SmogCloud() : TheThiefCard(-1, CardType.Skill, CardRarity.Uncommon, TargetType.AllEnemies), ICountdown
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PoisonPower>(8), new PowerVar<WeakPower>(1)];
+    public int MaxCount
+    {
+        get;
+        set
+        {
+            int changeBy = value - field;
+            field = value;
+            CurrentCount += changeBy;
+        }
+    } = 3;
+
+    public int CurrentCount
+    {
+        get => DynamicVars[nameof(CurrentCount)].IntValue;
+        set
+        {
+            DynamicVars[nameof(CurrentCount)].BaseValue = value;
+            if (DynamicVars[nameof(CurrentCount)].BaseValue < 0)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = 0;
+            }
+
+            if (DynamicVars[nameof(CurrentCount)].BaseValue > MaxCount)
+            {
+                DynamicVars[nameof(CurrentCount)].BaseValue = MaxCount;
+            }
+        }
+    }
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new PowerVar<PoisonPower>(5), new PowerVar<WeakPower>(1), new IntVar("CurrentCount", 3)];
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromPower<PoisonPower>(), HoverTipFactory.FromPower<WeakPower>()];
 
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
