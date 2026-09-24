db9f5c1330d04576c36339498783a6a6bd4c7cf2
Corey <coreySegiethMail@gmail.com>
2026-09-23 23:35:42 -0400

    Added Reroll. Removed Furious Form.

---
+++ Warrior/Common/Reroll.cs
@@ -0,0 +1,49 @@
+﻿using DiceTheSpire.Shared.Cards;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.HoverTips;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceTheSpire.Warrior.Common
+{
+    public class Reroll() : TheWarriorCard(1, CardType.Skill, CardRarity.Common, TargetType.Self)
+    {
+
+        protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(3)];
+        protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.FromCard<RollAgain>()];
+        protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+        {
+
+            if (CombatState is null)
+            {
+                return;
+            }
+
+
+            for (int n = 0; n < 3; ++n)
+            {
+                RollAgain card = CombatState.CreateCard<RollAgain>(Owner);
+                if (IsUpgraded)
+                {
+                    CardCmd.Upgrade(card);
+                }
+                await CardPileCmd.AddGeneratedCardToCombat(card, PileType.Hand, Owner);
+            }
+
+        }
+
+        protected override void OnUpgrade()
+        {
+            base.OnUpgrade();
+        }
+    }
+}
