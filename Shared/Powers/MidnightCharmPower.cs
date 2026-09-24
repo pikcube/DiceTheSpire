2e7c9baedbdaef6537427c981a968d967776121a
Cory Zink <cz109918@ohio.edu>
2026-09-23 23:33:51 -0400

    Finished Midnight Charm

---
+++ Shared/Powers/MidnightCharmPower.cs
@@ -0,0 +1,45 @@
+﻿using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Models;
+
+namespace DiceTheSpire.Shared.Powers;
+
+public class MidnightCharmPower : DiceTheSpirePower
+{
+    public override PowerType Type => PowerType.Buff;
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public override bool TryModifyEnergyCostInCombatLate(CardModel card, decimal originalCost, out decimal modifiedCost)
+    {
+        if (card.Owner.Creature != Owner || card.Pile?.Type != PileType.Hand)
+        {
+            modifiedCost = originalCost;
+            return false;
+        }
+
+        modifiedCost = 0;
+        return true;
+    }
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (cardPlay.Card.Owner.Creature != Owner || !cardPlay.IsLastInSeries || (cardPlay.IsAutoPlay && CardSelectCmd.Selector is null))
+        {
+            return;
+        }
+        
+        await PowerCmd.Decrement(this);
+    }
+
+    public override async Task AfterSideTurnEnd(PlayerChoiceContext choiceContext, CombatSide side, IEnumerable<Creature> participants)
+    {
+        if (side == Owner.Side)
+        {
+            await PowerCmd.Remove(this);
+        }
+    }
+}
--- Thief/Uncommon/MidnightCharm.cs
+++ Thief/Uncommon/MidnightCharm.cs
@@ -1,8 +1,28 @@
-﻿using MegaCrit.Sts2.Core.Entities.Cards;
+﻿using DiceTheSpire.Shared.Powers;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
 
 namespace DiceTheSpire.Thief.Uncommon;
 
-public class MidnightCharm() : TheThiefCard(2, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
+public class MidnightCharm() : TheThiefCard(3, CardType.Skill, CardRarity.Uncommon, TargetType.Self)
 {
-    
+    protected override IEnumerable<DynamicVar> CanonicalVars => [new CardsVar(2)];
+
+    public override async Task AfterCardPlayed(PlayerChoiceContext choiceContext, CardPlay cardPlay)
+    {
+        if (CombatState is null || cardPlay.Card != this)
+        {
+            return;
+        }
+
+        await PowerCmd.Apply<MidnightCharmPower>(choiceContext, Owner.Creature, DynamicVars.Cards.BaseValue,
+            Owner.Creature, this);
+    }
+
+    protected override void OnUpgrade()
+    {
+        EnergyCost.UpgradeBy(-1);
+    }
 }
