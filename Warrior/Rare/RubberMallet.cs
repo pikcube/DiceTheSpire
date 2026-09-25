9398c535af6eaea3e13d7d815a7690fc1d39af33
Corey <coreySegiethMail@gmail.com>
2026-09-25 00:39:01 -0400

    Fixed bug where rubber mallet damage would not reset between combats. Rubber Mallet now costs two.

--- Warrior/Rare/RubberMallet.cs
+++ Warrior/Rare/RubberMallet.cs
@@ -1,8 +1,10 @@
-﻿using DiceTheSpire.Shared.Commands;
+﻿using DiceTheSpire.Inventor.Rare;
+using DiceTheSpire.Shared.Commands;
 using DiceTheSpire.Shared.Listeners;
 using DiceTheSpire.Shared.Utility;
 using MegaCrit.Sts2.Core.Commands;
 using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
 using MegaCrit.Sts2.Core.GameActions.Multiplayer;
 using MegaCrit.Sts2.Core.HoverTips;
 using MegaCrit.Sts2.Core.Localization.DynamicVars;
@@ -11,38 +13,42 @@ using MegaCrit.Sts2.Core.Rooms;
 using MegaCrit.Sts2.Core.ValueProps;
 
 namespace DiceTheSpire.Warrior.Rare;
-public class RubberMallet() : TheWarriorCard(1, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy), IAfterRerollListener
+public class RubberMallet() : TheWarriorCard(2, CardType.Attack, CardRarity.Rare, TargetType.AnyEnemy), IAfterRerollListener
 {
-    protected override IEnumerable<DynamicVar> CanonicalVars => [new DamageVar(6, DamageProps.card), new ExtraDamageVar(2)];
+    public int RerolledThisCombat { get; set; }
+    protected override IEnumerable<DynamicVar> CanonicalVars => [.. MakeCalculatedDamage(6, Bonus, 2)]; 
     protected override IEnumerable<IHoverTip> ExtraHoverTips => [HoverTipFactory.Static(BetterStaticHoverTips.Reroll)];
 
-    //public override Task BeforeCombatStart()
-    //{
-    //    DynamicVars.Damage.ResetToBase();
-    //    return Task.CompletedTask;
-    //}
+    private static decimal Bonus(CardModel arg1, Creature? arg2)
+    {
+        return arg1 is not RubberMallet rm ? 1 : rm.RerolledThisCombat;
+    }
 
-
+    public override Task BeforeCombatStart()
+    {
+        RerolledThisCombat = 0;
+        return Task.CompletedTask;
+    }
     protected override async Task OnPlay(PlayerChoiceContext choiceContext, CardPlay cardPlay)
     {
         ArgumentNullException.ThrowIfNull(cardPlay.Target);
 
-        await DamageCmd.Attack(DynamicVars.Damage.EnchantedValue)
+        await DamageCmd.Attack(DynamicVars.CalculatedDamage)
             .FromCard(this, cardPlay)
             .Targeting(cardPlay.Target)
             .WithHitFx(VfxCmd.slashPath)
             .Execute(choiceContext);
-        
+
     }
 
     public Task AfterRerollAsync(PlayerChoiceContext choiceContext, CardModel card, bool isFixed, int originalCost, int getAmountToSpend, RerollDuration duration)
     {
-        DynamicVars.Damage.UpgradeValueBy(DynamicVars.ExtraDamage.BaseValue);
+        RerolledThisCombat++;
         return Task.CompletedTask;
     }
     protected override void OnUpgrade()
     {
-        DynamicVars.Damage.UpgradeValueBy(2);
+        DynamicVars.CalculationBase.UpgradeValueBy(3);
         DynamicVars.ExtraDamage.UpgradeValueBy(1);
     }
 }
